f7a25adcbdb303c96507a11dd00b31bbedc70844
Jakob Borg <jakob@nym.se>
2014-05-28 19:08:44 +0200

    Check for error in directory walker (ref #308)

--- model/puller.go
+++ model/puller.go
@@ -220,6 +220,10 @@ func (p *puller) fixupDirectories() {
 	var changed = 0
 
 	var walkFn = func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
 		if !info.IsDir() {
 			return nil
 		}
