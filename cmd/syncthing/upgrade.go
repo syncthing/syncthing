5a2328d9a5f6aca1ff3609c61788d7357d81df83
Jakob Borg <jakob@nym.se>
2014-06-07 03:56:13 +0200

    Build for Solaris

--- cmd/syncthing/upgrade.go
+++ cmd/syncthing/upgrade.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
+// +build !solaris
+
 package main
 
 import (
---
+++ cmd/syncthing/upgrade_solaris.go
@@ -0,0 +1,7 @@
+package main
+
+import "errors"
+
+func upgrade() error {
+	return errors.New("Upgrade currently unsupported on Solaris")
+}
