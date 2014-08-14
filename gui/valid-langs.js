2e7d6b2f9917a2dedad2aa429da0e3fba32e3ab9
Jakob Borg <jakob@nym.se>
2014-08-14 17:09:29 +0200

    Translation update, zh-CN

--- gui/valid-langs.js
+++ gui/valid-langs.js
@@ -1,1 +1,1 @@
-var validLangs = ["da","de","el","en","es","fr","hu","it","nl","pt-PT","ru","sv","tr","uk","zh-TW"]
+var validLangs = ["da","de","el","en","es","fr","hu","it","nl","pt-PT","ru","sv","tr","uk","zh-CN","zh-TW"]
