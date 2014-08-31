070b74fe6fda13d5d7cd96429923ea9f7feb8079
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2014-08-31 21:55:20 +0100

    Add tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -5,6 +5,11 @@
 package logger
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 )
@@ -55,5 +60,66 @@ func checkFunc(t *testing.T, expectl LogLevel, expectmsg string, counter *int) f
 		if !strings.HasSuffix(msg, expectmsg) {
 			t.Errorf("%q does not end with %q", msg, expectmsg)
 		}
+	}
+}
+
+func TestPanic(t *testing.T) {
+	bin, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		t.Error(err)
+	}
+	log := filepath.Join(filepath.Dir(bin), "panic.log")
+	os.Remove(log)
+
+	tests := map[string]func(){
+		"Test panic": func() { panic("Test panic") },
+		"runtime error: assignment to entry in nil map": func() {
+			var x map[int]int
+			x[1] = 1
+		},
+		"runtime error: index out of range": func() {
+			x := []int{
+				1: 1,
+			}
+			x[2] = 1
+		},
+	}
+
+	for msg, testfunc := range tests {
+		_, err = os.Stat(log)
+		if !os.IsNotExist(err) {
+			t.Error(err)
+		}
+
+		done := make(chan bool)
+		go func() {
+			defer func() {
+				r := recover()
+				if r == nil {
+					t.Error("Didn't repanic")
+				}
+				if fmt.Sprintf("%s", r) != msg {
+					t.Errorf("Incorrect repanic message: %s != %s", r, msg)
+				}
+				done <- true
+			}()
+			defer New().CaptureAndRepanic()
+			testfunc()
+		}()
+
+		<-done
+
+		bytes, err := ioutil.ReadFile(log)
+		if err != nil {
+			t.Error(err)
+		}
+		content := string(bytes)
+
+		if !strings.Contains(string(content), msg) {
+			t.Errorf("Does not contain '%s':\n%v", msg, content)
+		} else if !strings.Contains(string(content), "Stack trace:") {
+			t.Errorf("Does not contain 'Stack trace:':\n%v", content)
+		}
+		os.Remove(log)
 	}
 }
