d3a9b126a6fb423380154f2c2b4a0a9e04a44702
Jakob Borg <jakob@nym.se>
2014-10-08 13:58:41 +0200

    Translation update

--- gui/lang/valid-langs.js
+++ gui/lang/valid-langs.js
@@ -1,1 +1,1 @@
-var validLangs = ["bg","ca","da","de","el","en","es","fr","hu","it","lt","nl","pt-PT","ru","sv","tr","uk","zh-CN","zh-TW"]
+var validLangs = ["bg","de","en","fr","it","lt","pt-PT","sv","zh-CN","zh-TW"]
