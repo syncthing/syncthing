3b88ee623be8f99b8665ed27b6e875dead882644
Dennis Wilson <dw@risu.io>
2014-11-26 13:39:59 +0100

    GUI Rework: reorganized folders and split app.js

---
+++ gui/scripts/syncthing/core/directives/validDeviceidDirective.js
@@ -0,0 +1,23 @@
+angular.module('syncthing.core')
+    .directive('validDeviceid', function ($http) {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (scope.editingExisting) {
+                        // we shouldn't validate
+                        ctrl.$setValidity('validDeviceid', true);
+                    } else {
+                        $http.get(urlbase + '/deviceid?id=' + viewValue).success(function (resp) {
+                            if (resp.error) {
+                                ctrl.$setValidity('validDeviceid', false);
+                            } else {
+                                ctrl.$setValidity('validDeviceid', true);
+                            }
+                        });
+                    }
+                    return viewValue;
+                });
+            }
+        };
+});
