665b4506e77638a582d5b962838f660ffb44cc88
Jakob Borg <jakob@nym.se>
2014-12-08 15:41:08 +0100

    Correct check-contrib.sh

--- check-contrib.sh
+++ check-contrib.sh
@@ -13,7 +13,9 @@ no-docs-typos() {
 	grep -v f2459ef3319b2f060dbcdacd0c35a1788a94b8bd |\
 	grep -v b61f418bf2d1f7d5a9d7088a20a2a448e5e66801 |\
 	grep -v f0621207e3953711f9ab86d99724f1d0faac45b1 |\
-	grep -v f1120d7aa936c0658429edef0037792520b46334
+	grep -v f1120d7aa936c0658429edef0037792520b46334 |\
+	grep -v a9339d0627fff439879d157c75077f02c9fac61b |\
+	grep -v 254c63763a3ad42fd82259f1767db526cff94a14
 }
 
 print-missing-authors() {
@@ -28,10 +30,10 @@ print-missing-copyright() {
 
 authors=$(print-missing-authors)
 if [[ ! -z $authors ]] ; then
-	echo ***
+	echo '***'
 	echo Author emails not in AUTHORS:
 	echo $authors
-	echo ***
+	echo '***'
 	exit 1
 fi
 
