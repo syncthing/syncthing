e4837f14b11a0a23128e66a6df2d35cf200b57f2
Jakob Borg <jakob@nym.se>
2015-01-07 15:44:36 +0100

    Remove nil filenames from database and indexes (fixes #1243)

--- internal/db/leveldb.go
+++ internal/db/leveldb.go
@@ -586,6 +586,16 @@ func ldbWithAllFolderTruncated(db *leveldb.DB, folder []byte, fn func(device []b
 		if err != nil {
 			panic(err)
 		}
+
+		if f.Name == "" {
+			l.Infoln("Dropping invalid nil filename from database")
+			batch := new(leveldb.Batch)
+			ldbRemoveFromGlobal(db, batch, folder, device, nil)
+			batch.Delete(dbi.Key())
+			db.Write(batch, nil)
+			continue
+		}
+
 		if cont := fn(device, f); !cont {
 			return
 		}
