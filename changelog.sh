bf2e4a561a1e702d88f70638df1b2d80ae9c3fd0
Jakob Borg <jakob@nym.se>
2015-01-11 21:58:19 +0100

    Changelog prints output on two lines per commit

--- changelog.sh
+++ changelog.sh
@@ -5,5 +5,5 @@ if [[ -z $since ]] ; then
 	since="$(git describe --abbrev=0 HEAD^).."
 fi
 
-git log --pretty=format:'* %s (%h, @%aN)' "$since" | egrep 'fixes #\d|ref #\d'
+git log --pretty=format:'* %s  \n  (%h, @%aN)' "$since" | egrep 'fixes #\d|ref #\d' | sed s/\\\\n/\\$'\n'/
 
