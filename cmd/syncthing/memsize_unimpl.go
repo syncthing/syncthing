06b99144b9ee27d63350b9a3fdaaa40332cf1d30
sysadminmike <mike@wolman.co.uk>
2015-02-12 13:00:22 +0000

    Update memsize_unimpl.go

--- cmd/syncthing/memsize_unimpl.go
+++ cmd/syncthing/memsize_unimpl.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this program. If not, see <http://www.gnu.org/licenses/>.
 
-// +build freebsd openbsd
+// +build freebsd openbsd dragonfly
 
 package main
 
