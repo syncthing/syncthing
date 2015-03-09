d74f20615e7ac8e1462f3048e298886ec5569b53
stefan.seeland <stefan.seeland@gmx.at>
2015-03-09 18:36:30 +0100

    first draft - syncthingservicewrapper now gets started by installer

---
+++ SyncthingServiceWrapper/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace SyncthingServiceWrapper
+{
+    class WindowsService : ServiceBase
+    {
+        /// <summary>
+        /// Public Constructor for WindowsService.
+        /// - Put all of your Initialization code here.
+        /// </summary>
+        public WindowsService()
+        {
+            this.ServiceName = "Syncthing Service";
+            this.EventLog.Log = "Application";
+            this.EventLog.Source = this.ServiceName;
+
+            // These Flags set whether or not to handle that specific
+            //  type of event. Set to true if you need it, false otherwise.
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+        }
+
+        /// <summary>
+        /// The Main Thread: This is where your Service is Run.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase.Run(new WindowsService());
+        }
+
+        /// <summary>
+        /// Dispose of objects that need it here.
+        /// </summary>
+        /// <param name="disposing">Whether
+        ///    or not disposing is going on.</param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// OnStart(): Put startup code here
+        ///  - Start threads, get inital data, etc.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = Path.Combine(GetCurrentExecutingDirectory(), "syncthing.exe");
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            //p.StartInfo.Arguments = concatedParameterAndSources + "\"" + parameter.Destination.LocalPath + "\""; 
+            p.OutputDataReceived += new DataReceivedEventHandler(proc_OutputDataReceived);
+            p.ErrorDataReceived += new DataReceivedEventHandler(proc_ErrorDataReceived);
+            p.Start();
+
+            base.OnStart(args);
+        }
+
+        private void proc_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.EventLog.WriteEntry(e.Data);
+        }
+
+        private void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.EventLog.WriteEntry(e.Data);
+        }
+
+        private static string GetCurrentExecutingDirectory()
+        {
+            return Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath);
+        }
+
+        /// <summary>
+        /// OnStop(): Put your stop code here
+        /// - Stop threads, set final data, etc.
+        /// </summary>
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+
+        /// <summary>
+        /// OnPause: Put your pause code here
+        /// - Pause working threads, etc.
+        /// </summary>
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        /// <summary>
+        /// OnContinue(): Put your continue code here
+        /// - Un-pause working threads, etc.
+        /// </summary>
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        /// <summary>
+        /// OnShutdown(): Called when the System is shutting down
+        /// - Put code here when you need special handling
+        ///   of code that deals with a system shutdown, such
+        ///   as saving special data before shutdown.
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+
+        /// <summary>
+        /// OnCustomCommand(): If you need to send a command to your
+        ///   service without the need for Remoting or Sockets, use
+        ///   this method to do custom methods.
+        /// </summary>
+        /// <param name="command">Arbitrary Integer between 128 & 256</param>
+        protected override void OnCustomCommand(int command)
+        {
+            //  A custom command can be sent to a service by using this method:
+            //#  int command = 128; //Some Arbitrary number between 128 & 256
+            //#  ServiceController sc = new ServiceController("NameOfService");
+            //#  sc.ExecuteCommand(command);
+
+            base.OnCustomCommand(command);
+        }
+
+        /// <summary>
+        /// OnPowerEvent(): Useful for detecting power status changes,
+        ///   such as going into Suspend mode or Low Battery for laptops.
+        /// </summary>
+        /// <param name="powerStatus">The Power Broadcast Status
+        /// (BatteryLow, Suspend, etc.)</param>
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        /// <summary>
+        /// OnSessionChange(): To handle a change event
+        ///   from a Terminal Server session.
+        ///   Useful if you need to determine
+        ///   when a user logs in remotely or logs off,
+        ///   or when someone logs into the console.
+        /// </summary>
+        /// <param name="changeDescription">The Session Change
+        /// Event that occured.</param>
+        protected override void OnSessionChange(
+                  SessionChangeDescription changeDescription)
+        {
+            base.OnSessionChange(changeDescription);
+        }
+    }
+}
---
+++ WindowsSetup/Syncthing.xslt
@@ -0,0 +1,20 @@
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"   xmlns:msxsl="urn:schemas-microsoft-com:xslt"   exclude-result-prefixes="msxsl"   xmlns:wix="http://schemas.microsoft.com/wix/2006/wi"   xmlns:my="my:my"  xmlns:util="http://schemas.microsoft.com/wix/UtilExtension">
+  <!-- http://www.chriskonces.com/using-xslt-with-heat-exe-wix-to-create-windows-service-installs/ -->
+  <xsl:output method="xml" indent="yes" />
+  <xsl:strip-space elements="*" />
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()" />
+    </xsl:copy>
+  </xsl:template>
+  <xsl:template match="wix:Component[wix:File[@Source='$(var.MySource)\SyncthingServiceWrapper.exe']]">
+    <xsl:copy>
+      <xsl:apply-templates select="node() | @*" />
+      <!--<util:User Id="UpdateUserLogonAsService" UpdateIfExists="yes" CreateUser="no" Name="[SERVICECREDENTIALS_USERLOGIN]"
+            LogonAsService="yes" />-->
+      <wix:ServiceInstall Id="MyServiceInstall" DisplayName="Syncthing Service" Description="Syncthingservice created by installer" Name="Syncthing Service" ErrorControl="ignore" Start="auto" Type="ownProcess" Vital="yes" Interactive="no" Account="[SERVICECREDENTIALS_USERLOGIN]" Password="[SERVICECREDENTIALS_PASSWORD]" />
+      <wix:ServiceControl Id="MyServiceControl" Name="Syncthing Service" Start="install" Stop="both" Remove="uninstall" Wait="yes" />
+      <!--<util:User Id="user" CreateUser="no" Name ="[SERVICECREDENTIALS_USERLOGIN]" Password="[SERVICECREDENTIALS_PASSWORD]" LogonAsService="yes" />-->
+    </xsl:copy>
+  </xsl:template>
+</xsl:stylesheet>
