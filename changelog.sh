3e98bae5ec5f4fd9213a2e5510368f6a1ed4ddb3
Jakob Borg <jakob@nym.se>
2015-03-22 15:45:40 +0100

    Fix changelog.sh for Linux

--- changelog.sh
+++ changelog.sh
@@ -5,7 +5,16 @@ if [[ -z $since ]] ; then
 	since="$(git describe --abbrev=0 HEAD^).."
 fi
 
-git log --reverse --pretty=format:'* %s, @%aN)' "$since" | egrep 'fixes #\d|ref #\d' | sed 's/)[,. ]*,/,/' | sed 's/fixes #/#/g' | sed 's/ref #/#/g'
+case $(uname) in
+	Darwin)
+		grep="egrep"
+		;;
+	*)
+		grep="grep -P"
+		;;
+esac
+
+git log --reverse --pretty=format:'* %s, @%aN)' "$since" | $grep 'fixes #\d|ref #\d' | sed 's/)[,. ]*,/,/' | sed 's/fixes #/#/g' | sed 's/ref #/#/g'
 
 git diff "$since" -- AUTHORS
 
