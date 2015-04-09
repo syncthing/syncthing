cfc9776baefd28210239e958c68ce4d9b7230b4f
Jakob Borg <jakob@nym.se>
2015-04-09 12:53:13 +0200

    Check for short ID conflict at startup

--- cmd/syncthing/main_test.go
+++ cmd/syncthing/main_test.go
@@ -140,3 +140,27 @@ func TestFolderErrors(t *testing.T) {
 		t.Error("Unexpected error", cfg.Folders()["folder"].Invalid)
 	}
 }
+
+func TestShortIDCheck(t *testing.T) {
+	cfg := config.Wrap("/tmp/test", config.Configuration{
+		Devices: []config.DeviceConfiguration{
+			{DeviceID: protocol.DeviceID{8, 16, 24, 32, 40, 48, 56, 0, 0}},
+			{DeviceID: protocol.DeviceID{8, 16, 24, 32, 40, 48, 56, 1, 1}}, // first 56 bits same, differ in the first 64 bits
+		},
+	})
+
+	if err := checkShortIDs(cfg); err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	cfg = config.Wrap("/tmp/test", config.Configuration{
+		Devices: []config.DeviceConfiguration{
+			{DeviceID: protocol.DeviceID{8, 16, 24, 32, 40, 48, 56, 64, 0}},
+			{DeviceID: protocol.DeviceID{8, 16, 24, 32, 40, 48, 56, 64, 1}}, // first 64 bits same
+		},
+	})
+
+	if err := checkShortIDs(cfg); err == nil {
+		t.Error("Should have gotten an error")
+	}
+}
