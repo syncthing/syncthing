040a7f1cf4c5e1e3dedfc7cb60eba37cf2e29fd5
Jakob Borg <jakob@nym.se>
2015-05-28 14:17:54 +0200

    Spacing, deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -euo pipefail
 
-git pull && rm -fr _build && make html
+git pull
+rm -fr _build
+make html
+
+rm -rf _deployed.old
+[ -d _deployed ] && mv _deployed _deployed.old || true
+mv _build _deployed
 
