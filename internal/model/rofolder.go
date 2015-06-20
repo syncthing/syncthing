f73d5a9ab249be3f8c6b2f7ed4a2328d0f5d131b
Jakob Borg <jakob@nym.se>
2015-06-20 19:26:25 +0200

    Serialize scans and pulls (fixes #1391)

--- internal/model/rofolder.go
+++ internal/model/rofolder.go
@@ -22,7 +22,13 @@ type roFolder struct {
 	timer     *time.Timer
 	model     *Model
 	stop      chan struct{}
+	scanNow   chan rescanRequest
 	delayScan chan time.Duration
+}
+
+type rescanRequest struct {
+	subs []string
+	err  chan error
 }
 
 func newROFolder(model *Model, folder string, interval time.Duration) *roFolder {
@@ -36,6 +42,7 @@ func newROFolder(model *Model, folder string, interval time.Duration) *roFolder 
 		timer:     time.NewTimer(time.Millisecond),
 		model:     model,
 		stop:      make(chan struct{}),
+		scanNow:   make(chan rescanRequest),
 		delayScan: make(chan time.Duration),
 	}
 }
@@ -76,7 +83,7 @@ func (s *roFolder) Serve() {
 				l.Debugln(s, "rescan")
 			}
 
-			if err := s.model.ScanFolder(s.folder); err != nil {
+			if err := s.model.internalScanFolderSubs(s.folder, nil); err != nil {
 				// Potentially sets the error twice, once in the scanner just
 				// by doing a check, and once here, if the error returned is
 				// the same one as returned by CheckFolderHealth, though
@@ -92,10 +99,33 @@ func (s *roFolder) Serve() {
 			}
 
 			if s.intv == 0 {
-				return
+				continue
 			}
 
 			reschedule()
+
+		case req := <-s.scanNow:
+			if err := s.model.CheckFolderHealth(s.folder); err != nil {
+				l.Infoln("Skipping folder", s.folder, "scan due to folder error:", err)
+				req.err <- err
+				continue
+			}
+
+			if debug {
+				l.Debugln(s, "forced rescan")
+			}
+
+			if err := s.model.internalScanFolderSubs(s.folder, req.subs); err != nil {
+				// Potentially sets the error twice, once in the scanner just
+				// by doing a check, and once here, if the error returned is
+				// the same one as returned by CheckFolderHealth, though
+				// duplicate set is handled by setError.
+				s.setError(err)
+				req.err <- err
+				continue
+			}
+
+			req.err <- nil
 
 		case next := <-s.delayScan:
 			s.timer.Reset(next)
@@ -108,6 +138,15 @@ func (s *roFolder) Stop() {
 }
 
 func (s *roFolder) IndexUpdated() {
+}
+
+func (s *roFolder) Scan(subs []string) error {
+	req := rescanRequest{
+		subs: subs,
+		err:  make(chan error),
+	}
+	s.scanNow <- req
+	return <-req.err
 }
 
 func (s *roFolder) String() string {
