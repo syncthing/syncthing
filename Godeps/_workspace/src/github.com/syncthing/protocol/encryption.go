817d27a3d3bac5c087d5e647127f6607aaad835c
pascal257 <pascal.kuehne@gmail.com>
2015-07-27 02:49:04 +0200

    Implemeted decryption

--- Godeps/_workspace/src/github.com/syncthing/protocol/encryption.go
+++ Godeps/_workspace/src/github.com/syncthing/protocol/encryption.go
@@ -57,5 +57,33 @@ func Encrypt(buf []byte, label []byte, cert tls.Certificate) (out []byte, err er
 	return ret, nil
 }
 
+func Decrypt(buf []byte, label []byte, key *rsa.PrivateKey) (out []byte, err error) {
+	var ret []byte
+
+	l.Debugf("Trying to decrypt", len(buf), "bytes of data")
+
+	// now to encrypting
+	// each encrypted chunk may only be ((pubkey.N.BitLen() + 7) / 8) - 11 byte big, so we may have to cut her
+
+	k := 384
+
+	var offset int
+	
+	for i := 0; i < len(buf); i += k {
+		l.Debugf("Decrypt cicle i", i, "k", k, "i+k", i+k, "len", len(buf))
+		if i + k > len(buf) {
+			k = len(buf) - i
+		}
+		out, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, key, buf[i:i+k], label) // Outputs 384 Bytes
+		if err != nil {
+			l.Debugln("error:", err)
+			return nil, err
+		}
+
+		ret = append(ret, out...)
+
+		offset += len(out)
+	}
+
 	return ret, nil
 }
