8a1e699c617a9e0408d69c710f474d82d2641fe8
Jakob Borg <jakob@nym.se>
2015-09-13 11:44:33 +0200

    cmd/discosrv: New discovery protocol over HTTPS

--- cmd/discosrv/build.sh
+++ cmd/discosrv/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-set nullglob
-
-echo Get dependencies
-go get -d -tags purego
-
-rm -rf discosrv-*-*
 
 build() {
 	export GOOS="$1"
@@ -25,15 +19,33 @@ build() {
 	fi
 }
 
-for goos in linux darwin windows freebsd openbsd netbsd solaris ; do
-	build "$goos" amd64
-done
-for goos in linux windows freebsd openbsd netbsd ; do
-	build "$goos" 386
-done
-build linux arm
+buildpkg() {
+	echo Get dependencies
+	go get -d -tags purego
 
-# Hack used because we run as root under Docker
-if [[ ${CHOWN_USER:-} != "" ]] ; then
-	chown -R $CHOWN_USER .
-fi
+	rm -rf discosrv-*-*
+
+	for goos in linux darwin windows freebsd openbsd netbsd solaris ; do
+		build "$goos" amd64
+	done
+	for goos in linux windows freebsd openbsd netbsd ; do
+		build "$goos" 386
+	done
+	build linux arm
+
+	# Hack used because we run as root under Docker
+	if [[ ${CHOWN_USER:-} != "" ]] ; then
+		chown -R $CHOWN_USER .
+	fi
+}
+
+case "${1:-default}" in
+	pkg)
+		buildpkg
+		;;
+
+	default)
+		go install -v
+		;;
+esac
+
