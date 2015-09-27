f42f20c70cf5f0d87813ad31c379d66c7ff30a2e
simplypeachy <simplypeachy@users.noreply.github.com>
2015-09-27 09:50:54 +0100

    Update rwfolder.go
    Revert copyright/grammar changes.

--- lib/model/rwfolder.go
+++ lib/model/rwfolder.go
@@ -1,8 +1,8 @@
-// Copyright (C) 2015 The Syncthing Authors.
+// Copyright (C) 2014 The Syncthing Authors.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// you can obtain one at http://mozilla.org/MPL/2.0/.
+// You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package model
 
