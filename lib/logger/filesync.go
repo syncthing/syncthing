d82dfb81f9cb6e27027e0f194866d91073e0c154
Nate Morrison <natemorrison@gmail.com>
2015-11-29 23:11:42 -0500

    Simple daily logfile rotation added and changes to the log disclaimer

--- lib/logger/filesync.go
+++ lib/logger/filesync.go
@@ -46,14 +46,14 @@ func ResetUpdaterClient () {
 // This function records all the client index updates sent to the client
 // to a seperate log file than the debug log.
 func WriteClientSyncToLog(deviceID string, name string, numFiles int) {
-	f, eol := CreateLogFile("FileSync.log")
+	f, eol := CreateLogFile()
 	defer f.Close()
 	
 	// If the buffer's empty it's because the change was local, else we do something different (maybe in future)
 	if strings.Count(filesyncBuffer, eol) != 0 {
 		f.WriteString(updaterClient + eol)
 		f.WriteString(fmt.Sprintf("%s [%d object change(s) recieved]", name, numFiles) + eol)
-		f.WriteString("-------------------------------------------------------------------------------------" + eol)
+		//f.WriteString("--------------------------------------------------------------------------------" + eol)
 		
 		// Now write the buffer and clear it for the next time
 		f.WriteString(filesyncBuffer + eol)
@@ -64,20 +64,24 @@ func WriteClientSyncToLog(deviceID string, name string, numFiles int) {
 // This function records all the write file actions sent to the client
 // (delete / update) to a seperate log file than the debug log.
 func WriteFileSyncToLog(dataIf interface{}) {
-	f, eol := CreateLogFile("FileSync.log")
+	f, eol := CreateLogFile()
 	defer f.Close()
 	
 	// Don't write this right away but add it to the buffer to be written after the client info
 	data := dataIf.(map[string]interface{})
-	filesyncBuffer += time.Now().Format(time.StampMilli) + fmt.Sprintf(": [ %v ] %v %v \"%s\"", data["folder"], data["action"], data["type"], data["item"]) + eol
+	filesyncBuffer += time.Now().Format(time.StampMilli) + fmt.Sprintf(": [ %v ] --> %v %v \"%s\"", data["folder"], data["action"], data["type"], data["item"]) + eol
 	
 	f.Sync()
 }
 
 // Create the file if it doesn't exist and add disclaimer, otherwise just open it
 // and append to the end.  Also fixup the eol character for whichever OS we're on.
-func CreateLogFile(filename string) (*os.File, string) {
+func CreateLogFile() (*os.File, string) {
 	var f *os.File
+	
+	// Filename with datestamp for daily rotation of log
+	parts := strings.Split(time.Now().String(), " ")
+	filename := "FileSync_" + parts[0] + ".log"
 	
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		f, _ = os.Create(filename)
@@ -96,18 +100,21 @@ func CreateLogFile(filename string) (*os.File, string) {
 
 func LogInit() string {
 	initText := `LOG DISCLAIMER:
-Please understand this is a log of only what updates this computer recieves and
-from which other clients.  THIS LOG WILL NOT TELL YOU WHAT CHANGES ORIGINATED
-OUTBOUND FROM THIS COMPUTER!  Just what was propagated inbound to this computer
-by other computers and when.  That way if a file was deleted inadvertently a
-user could at least trace it back to the original computer by looking at these
-logs and any other nodes logs for other changes to know where the deletion came
-from.
-Also keep in mind this functionality assumes ALL your computers are exactly in
-time sync with each other.  If they are not, the timestamps in this log (using
-your local time zone not UTC) will be much less helpful at best and completely
-misleading at worse.  You are highly advised to use a time server on your
-network for all your nodes.
+THIS LOG WILL NOT TELL YOU WHAT CHANGES ORIGINATED OUTBOUND FROM THIS COMPUTER!
+This is a log of only what updates this computer recieves from other clients
+and when.  That way if a file was deleted inadvertently a user could possibly
+trace it back at least to the original computer by looking at these logs and
+any other nodes logs for other changes to know where the deletion came from.
+Also understand, due to the peer-to-peer nature of this program it is almost
+impossible to tell exactly where ALL parts of a file or folder came from.  When
+this log says a file came from a client it means that the request to send it
+was originated from that one.  Not that it was wholly recieved from that client
+only.
+Lastly, this functionality assumes ALL your computers are exactly in time sync
+with each other.  If they are not, the timestamps in this log (using your local
+time zone not UTC) will be much less helpful at best and completely misleading
+at worse.  You are highly advised to use a time server on your network for all
+your nodes if you plan to use these logs extensively.
 --------------------------------------------------------------------------------
 
 
