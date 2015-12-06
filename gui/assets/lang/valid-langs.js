1b6e4645b1a784fce05b78b841cd68f9aa266484
Jakob Borg <jakob@nym.se>
2015-12-06 13:09:22 +0100

    Update docs & translations

--- gui/assets/lang/valid-langs.js
+++ gui/assets/lang/valid-langs.js
@@ -1,1 +1,1 @@
-var validLangs = ["bg","ca","ca@valencia","cs","de","el","en","en-GB","es","es-ES","fi","fr","fr-CA","fy","hu","it","ja","ko-KR","lt","nb","nl","nn","pl","pt-BR","pt-PT","ro-RO","ru","sv","uk","zh-CN","zh-TW"]
+var validLangs = ["bg","ca","ca@valencia","cs","da","de","el","en","en-GB","es","es-ES","fi","fr","fr-CA","fy","hu","it","ja","ko-KR","lt","nb","nl","nn","pl","pt-BR","pt-PT","ro-RO","ru","sv","uk","zh-CN","zh-TW"]
---
+++ man/syncthing-relay.7
@@ -0,0 +1,7 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body bgcolor="white">
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx/1.7.4</center>
+</body>
+</html>
