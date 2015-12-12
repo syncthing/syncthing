be6410dca9120b05136232ee889ec63d0ca127d7
Lode Hoste <zillode@zillode.be>
2015-12-12 21:12:38 +0100

    S: Default nukes

---
+++ lib/ignore/nukes_darwin.go
@@ -0,0 +1,27 @@
+// Copyright (C) 2015 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package ignores
+
+const defaultIgnores = `
+._*
+.apdisk
+.AppleDB
+.AppleDesktop
+.AppleDouble
+.DocumentRevisions-V100
+.DS_Store
+.DS_Store?
+.fseventsd
+.LSOverride
+.Spotlight-V100
+.TemporaryItems
+Icon\r
+(?preserve).Trashes/
+(?preserve).Trash-*/
+(?preserve)Network Trash Folder/
+(?preserve)Temporary Items/
+`
---
+++ lib/ignore/nukes_windows.go
@@ -0,0 +1,19 @@
+// Copyright (C) 2015 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package ignores
+
+const defaultIgnores = `
+ehthumbs.db
+desktop.ini
+Thumbs.db
+(?preserve)lost+found
+(?preserve)$RECYCLE.BIN
+(?preserve)pagefile.sys
+(?preserve)System Volume Information
+(?preserve)swapfile.sys
+(?preserve)hiberfil.sys
+`
