9b1bebc9b289e3d85cb653d58982e64999dff501
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-03-09 12:43:16 +0000

    Correct path to genxdr after the change to Go1.5+ vendoring

--- lib/db/leveldb.go
+++ lib/db/leveldb.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//go:generate -command genxdr go run ../../Godeps/_workspace/src/github.com/calmh/xdr/cmd/genxdr/main.go
+//go:generate -command genxdr go run ../../vendor/github.com/calmh/xdr/cmd/genxdr/main.go
 //go:generate genxdr -o leveldb_xdr.go leveldb.go
 
 package db
