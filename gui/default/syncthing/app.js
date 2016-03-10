44f123739007367a4c87bc4424b4381ddb72202c
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-03-10 10:43:53 +0100

    fixed: wrong translation id (and added translation error log from: http://angular-translate.github.io/docs/#/guide/16_error-handling)

--- gui/default/syncthing/app.js
+++ gui/default/syncthing/app.js
@@ -57,6 +57,7 @@ syncthing.config(function ($httpProvider, $translateProvider, LocaleServiceProvi
         prefix: 'assets/lang/lang-',
         suffix: '.json'
     });
+    $translateProvider.useMissingTranslationHandlerLog();
 
     LocaleServiceProvider.setAvailableLocales(validLangs);
     LocaleServiceProvider.setDefaultLocale('en');
