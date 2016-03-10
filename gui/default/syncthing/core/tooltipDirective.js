0144e26de4ec70568e7d6cfc1854ce7fc7381c7e
norgeous <daniel@danielharte.co.uk>
2016-03-10 05:27:04 +0000

    bootstrap tooltips
    1. add an angular directive to apply js for bootstrap tooltips.
    2. convert all existing title="" to use new tooltip directive.
    
    ##normal tooltip example:
    ```<span title="hello">world</span>```
    
    ##using new tooltip directive:
    ```<span tooltip data-original-title="hello">world</span>```
    
    i believe bootstrap recommends using ```title``` attribute to populate
    the tooltips, but by using ```data-original-title``` the tooltips live
    update without reapplying the js code each time the content changes.
    
    This method also works well with angular expressions:
    ```data-original-title="{{'Download Rate' | translate}}"```
    This example provides a bootstrap tooltip saying 'Download Rate' that
    changes automatically when the language is updated.

---
+++ gui/default/syncthing/core/tooltipDirective.js
@@ -0,0 +1,12 @@
+angular.module('syncthing.core')
+    .directive('tooltip', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attributes) {
+                $(element).tooltip({
+                    html: 'true',
+                    placement: 'top'
+                });
+            }
+        };
+});
