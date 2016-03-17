043d134fbfb76c6e5c4a82e79cb6d0b088154da0
kc1212 <kc04bc@gmx.com>
2016-03-17 23:18:14 +0100

    Platform agnostic pathSeparator, better warning message

--- gui/default/syncthing/core/pathIsSubDirDirective.js
+++ gui/default/syncthing/core/pathIsSubDirDirective.js
@@ -2,14 +2,13 @@ angular.module('syncthing.core')
     .directive('pathIsSubDir', function () {
         return {
             require: 'ngModel',
-            priority: 1000,
             link: function (scope, elm, attrs, ctrl) {
                 ctrl.$parsers.unshift(function (viewValue) {
                     // This function checks whether xdir is a subdirectory of ydir,
                     // e.g. it would return true if xdir = "/home/a", ydir = "/home/a/b".
                     function isSubDir(xdir, ydir) {
-                        var xdirArr = xdir.split("/");
-                        var ydirArr = ydir.split("/");
+                        var xdirArr = xdir.split(scope.system.pathSeparator);
+                        var ydirArr = ydir.split(scope.system.pathSeparator);
                         if (xdirArr.slice(-1).pop() === "") {
                             xdirArr = xdirArr.slice(0, -1);
                         }
@@ -21,12 +20,11 @@ angular.module('syncthing.core')
                         }).every(function(e) { return e });
                     }
 
-
                     scope.pathIsSubFolder = false;
-                    scope.otherPath = "";
+                    scope.otherFolder = "";
                     for (var folderID in scope.folders) {
-                        scope.otherPath = scope.folders[folderID].path;
-                        if (isSubDir(scope.otherPath, viewValue)) {
+                        if (isSubDir(scope.folders[folderID].path, viewValue)) {
+                            scope.otherFolder = folderID;
                             scope.pathIsSubFolder = true;
                             break;
                         }
