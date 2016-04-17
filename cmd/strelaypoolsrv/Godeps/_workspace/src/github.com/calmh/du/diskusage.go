1bc4c1a8acc87d39d85f392695fc10133e12b2fb
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2016-04-17 14:49:00 +0100

    Screw godep

--- cmd/strelaypoolsrv/Godeps/_workspace/src/github.com/calmh/du/diskusage.go
+++
@@ -1,8 +0,0 @@
-package du
-
-// Usage holds information about total and available storage on a volume.
-type Usage struct {
-	TotalBytes int64 // Size of volume
-	FreeBytes  int64 // Unused size
-	AvailBytes int64 // Available to a non-privileged user
-}
