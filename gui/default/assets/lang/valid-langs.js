c513171014cd3b22ed442188569c59fb561efcd4
Jakob Borg <jakob@nym.se>
2016-05-26 09:49:07 +0200

    gui: Update translations

--- gui/default/assets/lang/valid-langs.js
+++ gui/default/assets/lang/valid-langs.js
@@ -1,1 +1,1 @@
-var validLangs = ["bg","ca","ca@valencia","cs","da","de","el","en","en-GB","es","es-ES","fi","fr","fr-CA","fy","hu","id","it","ja","lt","nb","nl","nn","pl","pt-BR","pt-PT","ru","sv","tr","uk","vi","zh-CN","zh-TW"]
+var validLangs = ["bg","ca","ca@valencia","cs","da","de","el","en","en-GB","es","es-ES","fi","fr","fr-CA","fy","hu","id","it","ja","ko-KR","lt","nb","nl","nn","pl","pt-BR","pt-PT","ru","sv","tr","uk","vi","zh-CN","zh-TW"]
