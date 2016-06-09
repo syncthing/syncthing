6b48949b427271603c59e858527973c8bb256858
Daniel Harte <daniel@harte.me>
2016-06-09 07:36:17 +0100

    gui: Add support for multiple stacked modals

--- gui/default/syncthing/core/modalDirective.js
+++ gui/default/syncthing/core/modalDirective.js
@@ -11,6 +11,37 @@ angular.module('syncthing.core')
                 icon: '@',
                 close: '@',
                 large: '@'
+            },
+            link: function (scope, element, attrs, tabsCtrl) {
+
+                // before modal show animation
+                $(element).on('show.bs.modal', function () {
+
+                    // cycle through open modals, acertain modal with highest z-index
+                    var largestZ = 1040;
+                    $('.modal:visible').each(function (i) {
+                        var thisZ = parseInt($(this).css('zIndex'));
+                        largestZ = thisZ > largestZ ? thisZ : largestZ;
+                    });
+
+                    // set this modal's z-index to be 10 above the highest z
+                    var aboveLargestZ = largestZ + 10;
+                    $(element).css('zIndex', aboveLargestZ);
+
+                    // set backdrop z-index. timeout used because element does not exist immediatly
+                    setTimeout(function () {
+                        $('.modal-backdrop:last').css('zIndex', aboveLargestZ-5);
+                    },0);
+
+                });
+
+                // after modal hide animation
+                $(element).on('hidden.bs.modal', function () {
+
+                    // reset z-index of modal to normal (backdrop element gets deleted, so no need to reset its z-index)
+                    $(element).css('zIndex', 1040);
+
+                });
             }
         };
     });
