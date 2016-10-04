a4b744aba9b7bb508e20a492bc5c505a00263b03
Xav <damajor@gmail.com>
2016-10-05 00:10:33 +0200

    Update to accept relay clients listening on all interfaces
    Listening on all interfaces is the default behavior of running the relay server without any parameters.
    This commit will allow those clients to connect instead of rejecting them with a HTTP-401.

--- cmd/strelaypoolsrv/main.go
+++ cmd/strelaypoolsrv/main.go
@@ -327,7 +327,7 @@ func handlePostRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// The client did not provide an IP address, use the IP address of the client.
-	if host == "" {
+	if host == "" && host == "0.0.0.0" {
 		uri.Host = net.JoinHostPort(rhost, port)
 		newRelay.URL = uri.String()
 	} else if host != rhost {
