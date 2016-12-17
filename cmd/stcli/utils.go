920a83ec7afaea3973628eb8c49c72428dc6d0fe
Jakob Borg <jakob@kastelo.net>
2016-12-17 10:51:48 +0100

    cmd/stcli: Fix metalint ineffasign complaint

--- cmd/stcli/utils.go
+++ cmd/stcli/utils.go
@@ -57,7 +57,7 @@ func prettyPrintJSON(json map[string]interface{}) {
 	}
 	sort.Strings(jsonKeys)
 	for _, k := range jsonKeys {
-		value := ""
+		var value string
 		rvalue := remap[k]
 		switch rvalue.(type) {
 		case int, int16, int32, int64, uint, uint16, uint32, uint64, float32, float64:
