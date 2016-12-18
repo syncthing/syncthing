b8c1c0e0488d36e7bfc6daee141a0904748ebc86
Jakob Borg <jakob@kastelo.net>
2016-12-18 21:07:44 +0000

    cmd/syncthing: Enable better crypto, print negotiated cipher suite
    This adds support for AES_256_GCM_SHA384 (in there since Go 1.5, a bit
    of a shame we missed it) and ChaCha20-Poly1305 (if built with Go 1.8;
    ignored on older Gos).
    
    GitHub-Pull-Request: https://github.com/syncthing/syncthing/pull/3822

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -620,6 +620,10 @@ func syncthingMain(runtimeOptions RuntimeOptions) {
 		InsecureSkipVerify:     true,
 		MinVersion:             tls.VersionTLS12,
 		CipherSuites: []uint16{
+			0xCCA8, // TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305, Go 1.8
+			0xCCA9, // TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, Go 1.8
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
