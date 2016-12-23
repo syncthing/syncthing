f7b36d3313e3bac0aa9b557a86d9cae1b8112c12
Heiko Zuerker <heiko@zuerker.org>
2016-12-23 16:37:52 -0600

    create new type receiveOnlyFolder

--- lib/model/receiveonlyfolder.go
+++ lib/model/receiveonlyfolder.go
@@ -7,10 +7,54 @@
 package model
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/syncthing/syncthing/lib/config"
+	"github.com/syncthing/syncthing/lib/fs"
+	"github.com/syncthing/syncthing/lib/sync"
+	"github.com/syncthing/syncthing/lib/versioner"
 )
+
+type receiveOnlyFolder struct {
+	sendReceiveFolder
+}
 
 func init() {
 	// WO folders are really just RW folders where we reject local changes...
-	folderFactories[config.FolderTypeReceiveOnly] = newSendReceiveFolder
+	folderFactories[config.FolderTypeReceiveOnly] = newReceiveOnlyFolder
+}
+
+func newReceiveOnlyFolder(model *Model, cfg config.FolderConfiguration, ver versioner.Versioner, mtimeFS *fs.MtimeFS) service {
+	f := &receiveOnlyFolder{
+		sendReceiveFolder {
+			folder: folder{
+				stateTracker: newStateTracker(cfg.ID),
+				scan:         newFolderScanner(cfg),
+				stop:         make(chan struct{}),
+				model:        model,
+			},
+			FolderConfiguration: cfg,
+
+			mtimeFS:   mtimeFS,
+			dir:       cfg.Path(),
+			versioner: ver,
+
+			queue:       newJobQueue(),
+			pullTimer:   time.NewTimer(time.Second),
+			remoteIndex: make(chan struct{}, 1), // This needs to be 1-buffered so that we queue a notification if we're busy doing a pull when it comes.
+
+			errorsMut: sync.NewMutex(),
+
+			initialScanCompleted: make(chan struct{}),
+		},
+	}
+
+	f.configureCopiersAndPullers()
+
+	return f
+}
+
+func (f *receiveOnlyFolder) String() string {
+	return fmt.Sprintf("receiveOnlyFolder/%s@%p", f.folderID, f)
 }
