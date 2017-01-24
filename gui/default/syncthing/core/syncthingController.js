7b0e0101788efd3fd5748aee0e1122b75251c73a
benshep <bjashepherd@gmail.com>
2017-01-24 15:09:38 +0000

    Check for zero
    Gets rid of nasty transient 'NaN' display on start of sync.

--- gui/default/syncthing/core/syncthingController.js
+++ gui/default/syncthing/core/syncthingController.js
@@ -733,7 +733,7 @@ angular.module('syncthing.core')
             }
 
             var bytes = $scope.model[folder].globalBytes - $scope.model[folder].inSyncBytes;
-            if (isNaN(bytes)) {
+            if (isNaN(bytes) || bytes == 0) {
                 return 0;
             }
             var k = 1024;
