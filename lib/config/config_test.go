e091762528bf5eb1356c0dd5fbf8454365fa3465
Jakob Borg <jakob@kastelo.net>
2017-01-26 12:36:45 +0100

    gui, lib/config: Add notification about new release channels

--- lib/config/config_test.go
+++ lib/config/config_test.go
@@ -200,7 +200,7 @@ func TestOverriddenValues(t *testing.T) {
 		AlwaysLocalNets:         []string{},
 		OverwriteRemoteDevNames: true,
 		TempIndexMinBlocks:      100,
-		UnackedNotificationIDs:  []string{},
+		UnackedNotificationIDs:  []string{"channelNotification"}, // added in 17->18 migration
 	}
 
 	cfg, err := Load("testdata/overridenvalues.xml", device1)
