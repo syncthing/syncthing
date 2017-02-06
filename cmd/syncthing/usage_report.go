ae1979d5121ab6df8719e6e31d7d2d76e4d7daa5
Jakob Borg <jakob@kastelo.net>
2017-02-06 13:39:00 +0100

    cmd/syncthing: Speed up the CPU benchmark
    Not the measured performance, just the wall clock time it takes to
    complete. The random generation was dominating.

--- cmd/syncthing/usage_report.go
+++ cmd/syncthing/usage_report.go
@@ -287,9 +287,13 @@ func (s *usageReportingService) Stop() {
 
 // cpuBench returns CPU performance as a measure of single threaded SHA-256 MiB/s
 func cpuBench(iterations int, duration time.Duration) float64 {
+	dataSize := 16 * protocol.BlockSize
+	bs := make([]byte, dataSize)
+	rand.Reader.Read(bs)
+
 	var perf float64
 	for i := 0; i < iterations; i++ {
-		if v := cpuBenchOnce(duration); v > perf {
+		if v := cpuBenchOnce(duration, bs); v > perf {
 			perf = v
 		}
 	}
@@ -299,17 +303,13 @@ func cpuBench(iterations int, duration time.Duration) float64 {
 
 var blocksResult []protocol.BlockInfo // so the result is not optimized away
 
-func cpuBenchOnce(duration time.Duration) float64 {
-	dataSize := 16 * protocol.BlockSize
-	bs := make([]byte, dataSize)
-	rand.Reader.Read(bs)
-
+func cpuBenchOnce(duration time.Duration, bs []byte) float64 {
 	t0 := time.Now()
 	b := 0
 	for time.Since(t0) < duration {
 		r := bytes.NewReader(bs)
-		blocksResult, _ = scanner.Blocks(r, protocol.BlockSize, int64(dataSize), nil, true)
-		b += dataSize
+		blocksResult, _ = scanner.Blocks(r, protocol.BlockSize, int64(len(bs)), nil, true)
+		b += len(bs)
 	}
 	d := time.Since(t0)
 	return float64(int(float64(b)/d.Seconds()/(1<<20)*100)) / 100
