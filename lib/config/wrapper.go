1f38f73177e5243098695843ff780d8a00d8ff7e
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2017-02-09 21:50:28 +0000

    Shuffle stun server addresses

--- lib/config/wrapper.go
+++ lib/config/wrapper.go
@@ -13,6 +13,7 @@ import (
 	"github.com/syncthing/syncthing/lib/events"
 	"github.com/syncthing/syncthing/lib/osutil"
 	"github.com/syncthing/syncthing/lib/protocol"
+	"github.com/syncthing/syncthing/lib/rand"
 	"github.com/syncthing/syncthing/lib/sync"
 	"github.com/syncthing/syncthing/lib/util"
 )
@@ -406,5 +407,6 @@ func (w *Wrapper) StunServers() []string {
 			addresses = append(addresses, addr)
 		}
 	}
+	rand.Shuffle(addresses)
 	return util.UniqueStrings(addresses)
 }
