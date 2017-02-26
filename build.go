138f1d85b3a819393314238793d76bb83caf573a
andresvia <andres.via@gmail.com>
2017-02-25 20:00:19 -0800

    build: moving deadcode

--- build.go
+++ build.go
@@ -204,6 +204,7 @@ func init() {
 }
 
 const minGoVersion = 1.5
+const minGoVendorVersion = 1.5
 
 func main() {
 	log.SetOutput(os.Stdout)
@@ -366,9 +367,10 @@ func checkRequiredGoVersion() (float64, bool) {
 			log.Printf("*** Couldn't parse Go version out of %q.\n*** This isn't known to work, proceed on your own risk.", vs)
 			return 0, false
 		}
-		if f < 1.5 {
+		if f < minGoVendorVersion {
 			log.Printf("*** Go version %.01f doesn't support the vendoring mechanism.\n*** Ensure correct dependencies in your $GOPATH.", f)
-		} else if f < minGoVersion {
+		}
+		if f < minGoVersion {
 			log.Fatalf("*** Go version %.01f is less than required %.01f.\n*** This is known not to work, not proceeding.", f, minGoVersion)
 		}
 		return f, true
