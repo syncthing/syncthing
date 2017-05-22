f4c484fe4657feb0636e103fc671f569dedac802
Simon Frei <freisim93@gmail.com>
2017-05-22 19:01:19 +0200

    Readd support for changed attributes on solaris as adviced upstream

--- lib/fswatcher/event_types_fen.go
+++ lib/fswatcher/event_types_fen.go
@@ -4,18 +4,18 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// +build !linux,!windows,!dragonfly,!freebsd,!netbsd,!openbsd,!solaris
-// +build !darwin darwin,cgo
-
-// Catch all platforms that are not specifically handled to use the generic
-// event types.
+// +build solaris,cgo
 
 package fswatcher
 
 import "github.com/zillode/notify"
 
 func (watcher *fsWatcher) eventMask() notify.Event {
-	return notify.All
+	events := notify.All
+	if !watcher.ignorePerms {
+		events |= notify.FileAttrib
+	}
+	return events
 }
 
 const removeEventMask = notify.Remove | notify.Rename
