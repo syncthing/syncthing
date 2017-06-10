098ac3c2bd103c46ad283af87cc8a5543638d2f1
nrm21 <natemorrison@gmail.com>
2017-06-10 18:58:03 -0400

    Get decryption portion working

--- lib/model/fileencryption.go
+++ lib/model/fileencryption.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/binary"
 	"encoding/hex"
@@ -20,43 +21,56 @@ import (
 	"github.com/syncthing/syncthing/lib/protocol"
 )
 
-// This function is used to encrypt or decrypt the file based on existance of the right file signature
-func EncOrDecFiles(rootDir string, key []byte, files []protocol.FileInfo) {
+// This function is used as wrapper to encrypt or decrypt the file based on
+// existance of the file signature and do some other setup first as well
+func (m *Model) EncOrDecFiles(folder string, files []protocol.FileInfo) {
+	rootDir := m.folderCfgs[folder].Path()
+
 	for _, file := range files {
 		if !file.IsDeleted() && !file.IsDirectory() { // non deleted files only
 			if file.Type == 0 || file.Type == 4 { // files and symlinks only (ignore dirs and all the other bs for now)
-				// some key for testing (32 bytes exactly, otherwise we need to truncate/pad to length)
+				key := m.getKeyFromConfig(folder)
 				key = stripOrPadKeyLength(key, 32)
-				l.Infoln("------------------------------------------------------------ key is:", key, len(key))
 				aesblock, err := aes.NewCipher(key)
 				if err != nil {
 					l.Infoln(err)
 				}
-				pathToFile := rootDir + string(os.PathSeparator) + filepath.FromSlash(file.Name)
 
+				pathToFile := rootDir + string(os.PathSeparator) + filepath.FromSlash(file.Name)
 				plainOrCiphertext, err := ioutil.ReadFile(pathToFile)
 				if err != nil {
 					l.Infoln(err)
 				}
 
-				// if file sig exists we should decrypt instead
-				if string(plainOrCiphertext[:5]) == "stenc" {
-					if !strings.HasSuffix(rootDir, ".ENC") { // make sure we're not running decryption dir created to hold the enc files
-						decryptFile(rootDir, pathToFile, key, aesblock, plainOrCiphertext)
+				if strings.HasSuffix(rootDir, ".ENC") { // make sure we're not running decryption in regular dir (only enc dir)
+					rootDir = rootDir[:len(rootDir)-4]
+					// now strip ".ENC" from folder path and find the folder ID for that path so we use the same key of the
+					// folder that encrypted the file.  This avoids an error where using 2 different keys causes either
+					// decyrption to fail or produce further gibberish which can be somewhat hard/confusing to debug
+					for id, fldrCfg := range m.folderCfgs {
+						if rootDir == fldrCfg.Path() { // rootDir now without the ".ENC"
+							folder = id
+							key = []byte(m.folderCfgs[folder].EncryptionKey)
+						}
 					}
-				} else {
-					encryptFile(rootDir, pathToFile, key, aesblock, plainOrCiphertext)
+
+					if string(plainOrCiphertext[:5]) == "stenc" { // if file sig exists we should decrypt
+						decryptFile(rootDir, pathToFile, aesblock, plainOrCiphertext)
+					}
+				} else { // otherwise we're in normal dir
+					encryptFile(rootDir, pathToFile, aesblock, plainOrCiphertext)
 				}
 			}
 		}
 	}
 }
 
-func decryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.Block, ciphertext []byte) {
+// Perform file decryption on ciphertext
+func decryptFile(rootDir string, pathToFile string, aesblock cipher.Block, ciphertext []byte) {
 	ciphertext = ciphertext[5:] // strip file sig
 
 	iv := ciphertext[:16]             // extract IV from next 16 bytes
-	ciphertext = ciphertext[len(iv):] // and strip it
+	ciphertext = ciphertext[len(iv):] // and strip IV
 
 	plaintext := make([]byte, len(ciphertext))
 	stream := cipher.NewCFBDecrypter(aesblock, iv)
@@ -66,23 +80,28 @@ func decryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.
 	if filepathNull == -1 {
 		l.Infoln("Cannot find null byte, we may have the wrong key or a corrupt file")
 	}
-	newfilepath := string(plaintext[:filepathNull]) // extract filepath
-	newfilepath = filepath.Base(newfilepath)
+	newFilepath := string(plaintext[:filepathNull]) // extract filepath
+	newFilepath = filepath.Base(newFilepath)
 	plaintext = plaintext[filepathNull+1:] // remove filepath from file contents
 	plaintext = plaintext[32:]             // strip off sha256 plaintext hash (since we're not using them right now)
 	plaintext = plaintext[(8 * 3):]        // strip off file access times (since we're not using them right now)
 
-	f, err := os.Create(rootDir + string(os.PathSeparator) + newfilepath)
-	if err != nil {
-		l.Infoln(err)
-	}
-	_, err = io.Copy(f, bytes.NewReader(plaintext))
-	if err != nil {
-		l.Infoln(err)
+	if _, err := os.Stat(rootDir + string(os.PathSeparator) + newFilepath); os.IsNotExist(err) {
+		f, err := os.Create(rootDir + string(os.PathSeparator) + newFilepath)
+		if err != nil {
+			l.Infoln(err)
+		}
+		_, err = io.Copy(f, bytes.NewReader(plaintext))
+		if err != nil {
+			l.Infoln(err)
+		}
+	} else {
+		l.Debugln("File already exists... ignoring decryption of file")
 	}
 }
 
-func encryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.Block, plaintext []byte) {
+// Perform file encryption on plaintext
+func encryptFile(rootDir string, pathToFile string, aesblock cipher.Block, plaintext []byte) {
 	// Get file MAC times and make byte arrays out of them
 	mtime, atime, ctime, err := fileStatTimes(pathToFile)
 	bmtime, batime, bctime := make([]byte, 8), make([]byte, 8), make([]byte, 8)
@@ -93,17 +112,23 @@ func encryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.
 
 	// The IV needs to be unique but not secure, so put it at beginning of ciphertext
 	h := sha256.New()
-	h.Write([]byte(pathToFile))
-	var iv []byte
-	iv = append(iv, h.Sum(nil)...)
+
+	// Never use more than 2^32 random nonces with given key because of risk of repeat
+	iv := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err.Error())
+	}
+	// h.Write([]byte(pathToFile))
+	// var iv []byte
+	// iv = append(iv, h.Sum(nil)...)
 
 	if _, err := io.Copy(h, bytes.NewReader(plaintext)); err != nil {
 		log.Fatal(err)
 	}
+
 	sha256HashBytes := h.Sum(nil) // sha256 of just the filedata by itself
 
-	var fileHeader []byte
-	fileHeader = append([]byte(pathToFile), 0)          // null byte terminated
+	fileHeader := append([]byte(pathToFile), 0)         // null byte terminated
 	fileHeader = append(fileHeader, sha256HashBytes...) // plaintext hash
 	fileHeader = append(fileHeader, bmtime...)          // mod time
 	fileHeader = append(fileHeader, batime...)          // access time
@@ -112,7 +137,7 @@ func encryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.
 	plaintext = append(fileHeader, plaintext...) // prepend fileheader to contents
 	ciphertext := make([]byte, len(plaintext))
 
-	iv = iv[:16] // make iv the first half of the sha256 hash
+	iv = iv[:16] // make sure we're using only the first 16 bytes of iv (if there happen to be more)
 	stream := cipher.NewCFBEncrypter(aesblock, iv)
 	stream.XORKeyStream(ciphertext, plaintext)
 
@@ -136,6 +161,26 @@ func encryptFile(rootDir string, pathToFile string, key []byte, aesblock cipher.
 		l.Infoln(err)
 	}
 	f.Close()
+}
+
+// This function should ensure we get the key from the same folderCfg despite working with 2 different folders
+func (m *Model) getKeyFromConfig(folder string) []byte {
+	rootDir := m.folderCfgs[folder].Path()
+	key := []byte(m.folderCfgs[folder].EncryptionKey)
+
+	if strings.HasSuffix(rootDir, ".ENC") { // make sure we're not running decryption in regular dir (only enc dir)
+		// now strip ".ENC" from folder path and find the folder ID for that path so we use the same key of the
+		// folder that encrypted the file.  This avoids an error where using 2 different keys causes either
+		// decyrption to fail or produce further gibberish which can be somewhat hard/confusing to debug
+		for id, fldrCfg := range m.folderCfgs {
+			if rootDir[:len(rootDir)-4] == fldrCfg.Path() { // rootDir without the ".ENC"
+				folder = id
+				key = []byte(m.folderCfgs[folder].EncryptionKey)
+			}
+		}
+	}
+
+	return key
 }
 
 // Enforce exactly 32 bytes long for the key
