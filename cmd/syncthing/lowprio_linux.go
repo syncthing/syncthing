b0e4bb042717e6106f86f7a489bbfe82ee4816d8
Jakob Borg <jakob@kastelo.net>
2017-12-31 16:23:12 +0100

    wip

--- cmd/syncthing/lowprio_linux.go
+++ cmd/syncthing/lowprio_linux.go
@@ -8,6 +8,29 @@ package main
 
 import "syscall"
 
+const IOPRIO_CLASS_SHIFT = 13
+
+type ioprioClass int
+
+const (
+	IOPRIO_CLASS_RT ioprioClass = iota + 1
+	IOPRIO_CLASS_BE
+	IOPRIO_CLASS_IDLE
+)
+
+const (
+	IOPRIO_WHO_PROCESS = iota + 1
+	IOPRIO_WHO_PGRP
+	IOPRIO_WHO_USER
+)
+
+func ioprioSet(class ioprioClass, value int) {
+	// error return ignored
+	syscall.Syscall(syscall.SYS_IOPRIO_SET,
+		uintptr(IOPRIO_WHO_PROCESS), 0,
+		uintptr(class)<<IOPRIO_CLASS_SHIFT|uintptr(value))
+}
+
 func setLowPriority() {
 	// Process zero is "self", niceness value 9 is something between 0
 	// (default) and 19 (worst priority). Error return ignored.
