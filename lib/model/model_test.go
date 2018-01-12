6e0f64017a5a24a9d83b781110a46ffb8b54b6e5
Jakob Borg <jakob@kastelo.net>
2018-01-12 08:26:33 +0100

    lib/logger, lib/model: Correct printf format strings in tests
    This is a build (test) failure in Go 1.10.

--- lib/model/model_test.go
+++ lib/model/model_test.go
@@ -3082,7 +3082,7 @@ func TestVersionRestore(t *testing.T) {
 			t.Errorf("unexpected %s", name)
 		}
 		if len(vers) != cnt {
-			t.Errorf("%s: %s != %s", name, cnt, len(vers))
+			t.Errorf("%s: %d != %d", name, cnt, len(vers))
 		}
 		// Delete, so we can check if we didn't hit something we expect afterwards.
 		delete(expectedVersions, name)
