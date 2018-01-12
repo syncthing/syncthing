178c3410a370bc768478ef3d97c9d4b58f2f0e37
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-01-12 22:52:29 +0100

    replaces stretchr/assert with gomega (already included)

--- lib/scanner/parallelhasher_test.go
+++ lib/scanner/parallelhasher_test.go
@@ -12,7 +12,7 @@ import (
 	gosync "sync"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/onsi/gomega"
 	"github.com/syncthing/syncthing/lib/fs"
 	"github.com/syncthing/syncthing/lib/protocol"
 	"github.com/syncthing/syncthing/test"
@@ -33,22 +33,24 @@ var (
 	outbox = make(chan protocol.FileInfo)
 	inbox  = make(chan protocol.FileInfo)
 	done   = make(chan struct{})
+	h      = newParallelHasher(hConfig, 100, outbox, inbox, done)
 )
 
 func Test_shouldCallExactNumberOfWorkers(t *testing.T) {
-	h := newParallelHasher(hConfig, 100, outbox, inbox, done)
+	g := gomega.NewGomegaWithT(t)
 
 	countedWaitGroup := &countedWaitGroup{}
 	h.wg = countedWaitGroup
 
 	h.run(context.TODO(), &noGlobalFolderScannerLimiter{})
 
-	assert.Equal(t, 100, countedWaitGroup.count)
+	g.Expect(100, countedWaitGroup.count)
 }
 
 func Test_shouldHashTestFile(t *testing.T) {
 	tempDir := test.NewTemporaryDirectoryForTests()
 	defer tempDir.Cleanup()
+	g := gomega.NewGomegaWithT(t)
 
 	hConfig.filesystem = fs.NewFilesystem(fs.FilesystemTypeBasic, ".")
 
@@ -66,7 +68,7 @@ func Test_shouldHashTestFile(t *testing.T) {
 
 	// verify
 	firstBlock := finfo.Blocks[0]
-	assert.Equal(t, protocol.BlockInfo{
+	g.Expect(protocol.BlockInfo{
 		Size: int32(4),
 		Hash: []uint8{0x9f, 0x86, 0xd0, 0x81, 0x88, 0x4c, 0x7d, 0x65, 0x9a, 0x2f, 0xea, 0xa0, 0xc5, 0x5a, 0xd0, 0x15, 0xa3, 0xbf, 0x4f, 0x1b, 0x2b, 0xb, 0x82, 0x2c, 0xd1, 0x5d, 0x6c, 0x15, 0xb0, 0xf0, 0xa, 0x8},
 	}, firstBlock)
