628ae2b585bd53a3832ae30d63eb8ff411eea2a9
Pawel Palenica <pawelpalenica11@gmail.com>
2018-02-10 15:13:55 +0100

    code review adjustments

--- lib/connections/limiter_test.go
+++ lib/connections/limiter_test.go
@@ -16,29 +16,44 @@ import (
 
 var device1, device2, device3, device4 protocol.DeviceID
 var dev1Conf, dev2Conf, dev3Conf, dev4Conf config.DeviceConfiguration
-var cfg *config.Wrapper
 
 func init() {
 	device1, _ = protocol.DeviceIDFromString("AIR6LPZ7K4PTTUXQSMUUCPQ5YWOEDFIIQJUG7772YQXXR5YD6AWQ")
 	device2, _ = protocol.DeviceIDFromString("GYRZZQB-IRNPV4Z-T7TC52W-EQYJ3TT-FDQW6MW-DFLMU42-SSSU6EM-FBK2VAY")
 	device3, _ = protocol.DeviceIDFromString("LGFPDIT-7SKNNJL-VJZA4FC-7QNCRKA-CE753K7-2BW5QDK-2FOZ7FR-FEP57QJ")
 	device4, _ = protocol.DeviceIDFromString("P56IOI7-MZJNU2Y-IQGDREY-DM2MGTI-MGL3BXN-PQ6W5BM-TBBZ4TJ-XZWICQ2")
+}
 
-	cfg = config.Wrap("/dev/null", config.New(device1))
+func initConfig() *config.Wrapper{
+	cfg := config.Wrap("/dev/null", config.New(device1))
+	dev1Conf = config.NewDeviceConfiguration(device1, "device1")
+	dev2Conf = config.NewDeviceConfiguration(device2, "device2")
+	dev3Conf = config.NewDeviceConfiguration(device3, "device3")
+	dev4Conf = config.NewDeviceConfiguration(device4, "device4")
+
+	dev2Conf.MaxRecvKbps = rand.Int() % 100000
+	dev2Conf.MaxSendKbps = rand.Int() % 100000
+
+	waiter, _ := cfg.SetDevices([]config.DeviceConfiguration{dev1Conf, dev2Conf, dev3Conf, dev4Conf})
+	waiter.Wait()
+	return cfg
 }
 
 func TestLimiterInit(t *testing.T) {
-	initConfig()
+	cfg := initConfig()
 	lim := newLimiter(cfg)
 
+	device2ReadLimit := dev2Conf.MaxRecvKbps
+	device2WriteLimit := dev2Conf.MaxSendKbps
+
 	expectedR := map[protocol.DeviceID]*rate.Limiter{
-		device2: rate.NewLimiter(rate.Limit(dev2Conf.MaxRecvKbps*1024), limiterBurstSize),
+		device2: rate.NewLimiter(rate.Limit(device2ReadLimit*1024), limiterBurstSize),
 		device3: rate.NewLimiter(rate.Inf, limiterBurstSize),
 		device4: rate.NewLimiter(rate.Inf, limiterBurstSize),
 	}
 
 	expectedW := map[protocol.DeviceID]*rate.Limiter{
-		device2: rate.NewLimiter(rate.Limit(dev2Conf.MaxSendKbps*1024), limiterBurstSize),
+		device2: rate.NewLimiter(rate.Limit(device2WriteLimit*1024), limiterBurstSize),
 		device3: rate.NewLimiter(rate.Inf, limiterBurstSize),
 		device4: rate.NewLimiter(rate.Inf, limiterBurstSize),
 	}
@@ -50,7 +65,7 @@ func TestLimiterInit(t *testing.T) {
 }
 
 func TestSetDeviceLimits(t *testing.T) {
-	initConfig()
+	cfg := initConfig()
 	lim := newLimiter(cfg)
 
 	// should still be inf/inf because this is local device
@@ -88,7 +103,7 @@ func TestSetDeviceLimits(t *testing.T) {
 }
 
 func TestRemoveDevice(t *testing.T) {
-	initConfig()
+	cfg := initConfig()
 	lim := newLimiter(cfg)
 
 	waiter, _ := cfg.RemoveDevice(device3)
@@ -108,7 +123,7 @@ func TestRemoveDevice(t *testing.T) {
 }
 
 func TestAddDevice(t *testing.T) {
-	initConfig()
+	cfg := initConfig()
 	lim := newLimiter(cfg)
 
 	addedDevice, _ := protocol.DeviceIDFromString("XZJ4UNS-ENI7QGJ-J45DT6G-QSGML2K-6I4XVOG-NAZ7BF5-2VAOWNT-TFDOMQU")
@@ -139,7 +154,7 @@ func TestAddDevice(t *testing.T) {
 }
 
 func TestAddAndRemove(t *testing.T) {
-	initConfig()
+	cfg := initConfig()
 	lim := newLimiter(cfg)
 
 	addedDevice, _ := protocol.DeviceIDFromString("XZJ4UNS-ENI7QGJ-J45DT6G-QSGML2K-6I4XVOG-NAZ7BF5-2VAOWNT-TFDOMQU")
@@ -169,18 +184,6 @@ func TestAddAndRemove(t *testing.T) {
 	checkActualAndExpected(t, actualR, actualW, expectedR, expectedW)
 }
 
-func initConfig() {
-	dev1Conf = config.NewDeviceConfiguration(device1, "device1")
-	dev2Conf = config.NewDeviceConfiguration(device2, "device2")
-	dev3Conf = config.NewDeviceConfiguration(device3, "device3")
-	dev4Conf = config.NewDeviceConfiguration(device4, "device4")
-
-	dev2Conf.MaxRecvKbps = rand.Int() % 100000
-	dev2Conf.MaxSendKbps = rand.Int() % 100000
-
-	waiter, _ := cfg.SetDevices([]config.DeviceConfiguration{dev1Conf, dev2Conf, dev3Conf, dev4Conf})
-	waiter.Wait()
-}
 
 func checkActualAndExpected(t *testing.T, actualR, actualW, expectedR, expectedW map[protocol.DeviceID]*rate.Limiter) {
 	t.Helper()
