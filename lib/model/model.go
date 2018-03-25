4537bcb627650cea2f269026e3f867bd7dec04f1
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2018-03-25 20:45:09 +0100

    Move invalid check

--- lib/model/model.go
+++ lib/model/model.go
@@ -1788,15 +1788,16 @@ func (m *Model) updateLocals(folder string, fs []protocol.FileInfo) {
 
 func (m *Model) diskChangeDetected(folderCfg config.FolderConfiguration, files []protocol.FileInfo, typeOfEvent events.EventType) {
 	for _, file := range files {
+		if file.IsInvalid() {
+			continue
+		}
+
 		objType := "file"
 		action := "modified"
 
 		switch {
 		case file.IsDeleted():
 			action = "deleted"
-
-		case file.IsInvalid():
-			continue
 
 		// If our local vector is version 1 AND it is the only version
 		// vector so far seen for this file then it is a new file.  Else if
