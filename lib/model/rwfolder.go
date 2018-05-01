c12521508f3069d9889efe1a6db64d0e72841688
Jakob Borg <jakob@kastelo.net>
2018-05-01 23:18:06 +0200

    lib/model: Units are hard (fixes #4910)

--- lib/model/rwfolder.go
+++ lib/model/rwfolder.go
@@ -81,7 +81,7 @@ const (
 const (
 	defaultCopiers          = 2
 	defaultPullerPause      = 60 * time.Second
-	defaultPullerPendingKiB = 2 * protocol.MaxBlockSize
+	defaultPullerPendingKiB = 2 * protocol.MaxBlockSize / 1024
 
 	maxPullerIterations = 3
 )
