301bd46a1ee3dc31dafca7c4d9e828c8e5bfaaab
Simon Frei <freisim93@gmail.com>
2018-06-02 02:39:15 +0200

    review

--- gui/default/syncthing/core/percentFilter.js
+++ gui/default/syncthing/core/percentFilter.js
@@ -6,7 +6,7 @@ angular.module('syncthing.core')
                 return 0 + '%';
             }
             // Hard limit at two decimals
-            if (input === undefined || input < 0.1) {
+            if (input < 0.1) {
                 return input.toLocaleString(undefined, {maximumFractionDigits: 2}) + '%';
             }
             // "Soft" limit at two significant digits (e.g. 1.2%, not 1.27%)
