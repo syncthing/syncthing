b5e8bf3f5873671aab94dac4af77590323018a96
Ian Johnson <person.uwsome@gmail.com>
2018-06-26 14:41:53 -0500

    build.go: during snap build, set i386 snap architecture to 386 goarch

--- build.go
+++ build.go
@@ -618,6 +618,8 @@ func buildSnap(target target) {
 	snaparch := goarch
 	if snaparch == "armhf" {
 		goarch = "arm"
+	} else if snaparch == "i386" {
+		goarch = "386"
 	}
 	snapver := version
 	if strings.HasPrefix(snapver, "v") {
