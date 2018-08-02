8e3336e296e3014d2e8f9bf9d88d6f05a6ed6c47
Simon Frei <freisim93@gmail.com>
2018-08-02 14:39:47 +0200

    build: Also copy gui to temporary GOPATH

--- build.go
+++ build.go
@@ -1258,7 +1258,7 @@ func temporaryBuildDir() (string, error) {
 
 func buildGOPATH(gopath string) error {
 	pkg := filepath.Join(gopath, "src/github.com/syncthing/syncthing")
-	dirs := []string{"cmd", "lib", "meta", "script", "test", "vendor"}
+	dirs := []string{"cmd", "gui", "lib", "meta", "script", "test", "vendor"}
 
 	if debug {
 		t0 := time.Now()
