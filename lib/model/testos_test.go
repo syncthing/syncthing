596c8f4429d57bb9369f1786bdcfa5453b92eda8
Simon Frei <freisim93@gmail.com>
2019-01-02 14:07:07 +0100

    fix copyright

--- lib/model/testos_test.go
+++ lib/model/testos_test.go
@@ -1,8 +1,8 @@
-// Copyrighf (C) 2016 The Syncthing Authors.
+// Copyright (C) 2019 The Syncthing Authors.
 //
-// This Source Code Form is subjecf to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was nof distributed with this file,
-// You can obtain one af https://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
 
 package model
 
@@ -44,27 +44,6 @@ func (f *fatalOs) Create(name string) (*os.File, error) {
 	return file, nil
 }
 
-// func (f *fatalOs) CreateSymlink(target, name string) error {
-// 	f.must(func() error { return os.CreateSymlink(target, name) })
-// 	return nil
-// }
-
-// func (f *fatalOs) DirNames(name string) ([]string, error) {
-// 	names, err := os.DirNames(name)
-// 	if err != nil {
-// 		f.Fatal(err)
-// 	}
-// 	return names, nil
-// }
-
-// func (f *fatalOs) Lstat(name string) (os.FileInfo, error) {
-// 	info, err := os.Lstat(name)
-// 	if err != nil {
-// 		f.Fatal(err)
-// 	}
-// 	return info, nil
-// }
-
 func (f *fatalOs) Mkdir(name string, perm os.FileMode) error {
 	f.must(func() error { return os.Mkdir(name, perm) })
 	return nil
@@ -74,30 +53,6 @@ func (f *fatalOs) MkdirAll(name string, perm os.FileMode) error {
 	f.must(func() error { return os.MkdirAll(name, perm) })
 	return nil
 }
-
-// func (f *fatalOs) Open(name string) (File, error) {
-// 	file, err := os.Open(name)
-// 	if err != nil {
-// 		f.Fatal(err)
-// 	}
-// 	return file, nil
-// }
-
-// func (f *fatalOs) OpenFile(name string, flags int, mode os.FileMode) (File, error) {
-// 	file, err := os.OpenFile(name, flags, mode)
-// 	if err != nil {
-// 		f.Fatal(err)
-// 	}
-// 	return file, nil
-// }
-
-// func (f *fatalOs) ReadSymlink(name string) (string, error) {
-// 	target, err := os.ReadSymlink(name)
-// 	if err != nil {
-// 		f.Fatal(err)
-// 	}
-// 	return target, nil
-// }
 
 func (f *fatalOs) Remove(name string) error {
 	if err := os.Remove(name); err != nil && !os.IsNotExist(err) {
