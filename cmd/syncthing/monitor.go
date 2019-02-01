647064b5207b9ea4efae90e53fb7dd6a292e467f
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-02-02 00:16:37 +0300

    cmd/syncthing: fix strings.HasPrefix args order
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- cmd/syncthing/monitor.go
+++ cmd/syncthing/monitor.go
@@ -418,10 +418,10 @@ func (f *autoclosedFile) closerLoop() {
 func childEnv() []string {
 	var env []string
 	for _, str := range os.Environ() {
-		if strings.HasPrefix("STNORESTART=", str) {
+		if strings.HasPrefix(str, "STNORESTART=") {
 			continue
 		}
-		if strings.HasPrefix("STMONITORED=", str) {
+		if strings.HasPrefix(str, "STMONITORED=") {
 			continue
 		}
 		env = append(env, str)
