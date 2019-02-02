860a0583567b699642d1c037fa22950b95f13027
Jakob Borg <jakob@kastelo.net>
2019-02-02 13:23:56 +0100

    wip

--- lib/config/encryption.go
+++ lib/config/encryption.go
@@ -7,15 +7,13 @@
 package config
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/xml"
 	"errors"
-	"fmt"
 	"io"
-	"strings"
-
-	"golang.org/x/crypto/nacl/secretbox"
 )
 
 var (
@@ -63,35 +61,54 @@ func (s *encryptedString) UnmarshalXML(d *xml.Decoder, start xml.StartElement) e
 }
 
 func obfuscate(data string) string {
-	var nonce [24]byte
-	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
-		panic(errors.New("randomness disaster: " + err.Error()))
+	block, err := aes.NewCipher(encryptionKey[:])
+	if err != nil {
+		panic("impossible error:" + err.Error())
 	}
-	out := secretbox.Seal(nil, []byte(data), &nonce, &encryptionKey)
-	return fmt.Sprintf("%s/%s", base64.RawURLEncoding.EncodeToString(nonce[:]), base64.RawURLEncoding.EncodeToString(out))
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic("impossible error:" + err.Error())
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		panic("impossible error:" + err.Error())
+	}
+
+	out := gcm.Seal(nonce, nonce, []byte(data), nil)
+	return base64.RawURLEncoding.EncodeToString(out)
 }
 
 func deobfuscate(data string) (string, bool) {
-	parts := strings.Split(data, "/")
-	if len(parts) != 2 {
-		return "", false
-	}
-
-	var nonce [24]byte
-	if _, err := base64.RawURLEncoding.Decode(nonce[:], []byte(parts[0])); err != nil {
-		return "", false
-	}
-
-	box, err := base64.RawURLEncoding.DecodeString(parts[1])
+	ciphertext, err := base64.RawURLEncoding.DecodeString(data)
 	if err != nil {
 		return "", false
 	}
 
-	msg, ok := secretbox.Open(nil, box, &nonce, &encryptionKey)
-	if !ok {
+	block, err := aes.NewCipher(encryptionKey[:])
+	if err != nil {
 		return "", false
 	}
-	return string(msg), true
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", false
+	}
+	if len(ciphertext) < gcm.NonceSize() {
+		return "", false
+	}
+
+	out, err := gcm.Open(nil,
+		ciphertext[:gcm.NonceSize()],
+		ciphertext[gcm.NonceSize():],
+		nil,
+	)
+	if err != nil {
+		return "", false
+	}
+	return string(out), true
 }
 
 type xmlEncryptedString struct {
