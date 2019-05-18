14243ac66b850cf45faea69373e5e1bb23462c9b
Jakob Borg <jakob@kastelo.net>
2019-05-18 08:38:42 +0200

    cleanup

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -5,7 +5,4 @@ set -eu
 chown "${PUID}:${PGID}" /var/syncthing \
   && exec su-exec "${PUID}:${PGID}" \
      env HOME=/var/syncthing \
-     /bin/syncthing \
-     -home /var/syncthing/config \
-     -gui-address http://0.0.0.0:8443/ \
-     "$@"
+     /bin/syncthing "$@"
