a1e3338812054a6873e8d7cb36d2cc2fe58cc448
André Colomb <src@andre.colomb.de>
2019-05-28 22:08:17 +0200

    lib/config: Add data structures to remember candidate devices.
    Modeled after the pending device mechanism, the new
    ObservedCandidateDevice structure stores information about remote
    devices which were advertised by other remote nodes.
    
    From the ClusterConfig message, it can collect the advertised certName
    and the address list.  A list of introducers may be included, reusing
    the ObservedDevice structure type.  In contrast to the pending device
    entries, the Name field should hold the candidate device's name as the
    introducer calls it, not the candidate's own advertised name.
    
    Functions for maintaining duplicate-free introducer entries and
    address lists are defined along with the structure type.

--- lib/config/observed.go
+++ lib/config/observed.go
@@ -24,3 +24,53 @@ type ObservedDevice struct {
 	Name    string            `xml:"name,attr" json:"name"`
 	Address string            `xml:"address,attr,omitempty" json:"address"`
 }
+
+type ObservedCandidateDevice struct {
+	ID           protocol.DeviceID `xml:"id,attr" json:"deviceID"`
+	CertName     string            `xml:"certName,attr,omitempty" json:"certName"`
+	Addresses    []string          `xml:"address,omitempty" json:"addresses"`
+	IntroducedBy []ObservedDevice  `xml:"introducedBy,omitempty" json:"introducedBy"`
+}
+
+// Generate a map of remote devices who introduced this candidate, keyed by device ID
+func (d *ObservedCandidateDevice) Introducers() map[protocol.DeviceID]ObservedDevice {
+	introducerMap := make(map[protocol.DeviceID]ObservedDevice, len(d.IntroducedBy))
+	for _, dev := range d.IntroducedBy {
+		introducerMap[dev.ID] = dev
+	}
+	return introducerMap
+}
+
+// Add or update information from an introducer to the candidate device description
+func (d *ObservedCandidateDevice) SetIntroducer(introducer protocol.DeviceID, name string) {
+	// Sort devices who introduced this candidate into a map for deduplication
+	introducerMap := d.Introducers()
+	introducerMap[introducer] = ObservedDevice{
+		Time: time.Now().Round(time.Second),
+		ID:   introducer,
+		Name: name,
+	}
+	d.IntroducedBy = make([]ObservedDevice, 0, len(introducerMap))
+	for _, n := range introducerMap {
+		d.IntroducedBy = append(d.IntroducedBy, n)
+	}
+}
+
+// Collect addresses to try for contacting a candidate device later
+func (d *ObservedCandidateDevice) CollectAddresses(addresses []string) {
+	if len(addresses) == 0 {
+		return
+	}
+	// Sort addresses into a map for deduplication
+	addressMap := make(map[string]struct{}, len(d.Addresses))
+	for _, s := range d.Addresses {
+		addressMap[s] = struct{}{}
+	}
+	for _, s := range addresses {
+		addressMap[s] = struct{}{}
+	}
+	d.Addresses = make([]string, 0, len(addressMap))
+	for a, _ := range addressMap {
+		d.Addresses = append(d.Addresses, a)
+	}
+}
