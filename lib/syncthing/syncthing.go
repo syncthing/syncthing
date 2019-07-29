7fc6c07c2e64706249a1a623f6fad9f7ff50cede
Simon Frei <freisim93@gmail.com>
2019-07-29 08:39:07 +0200

    naming

--- lib/syncthing/syncthing.go
+++ lib/syncthing/syncthing.go
@@ -101,7 +101,7 @@ func (a *App) Run() ExitStatus {
 func (a *App) Start() {
 	a.startOnce.Do(func() {
 		if err := a.startup(); err != nil {
-			a.stop(ExitError, err)
+			a.stopWithErr(ExitError, err)
 			return
 		}
 		go a.run()
@@ -396,10 +396,10 @@ func (a *App) Error() error {
 // Stop stops the app and sets its exit status to given reason, unless the app
 // was already stopped before. In any case it returns the effective exit status.
 func (a *App) Stop(stopReason ExitStatus) ExitStatus {
-	return a.stop(stopReason, nil)
+	return a.stopWithErr(stopReason, nil)
 }
 
-func (a *App) stop(stopReason ExitStatus, err error) ExitStatus {
+func (a *App) stopWithErr(stopReason ExitStatus, err error) ExitStatus {
 	a.stopOnce.Do(func() {
 		// ExitSuccess is the default value for a.exitStatus. If another status
 		// was already set, ignore the stop reason given as argument to Stop.
