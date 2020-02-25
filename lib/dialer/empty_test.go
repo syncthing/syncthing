c51ad4ff6e4e808698a9cda64888d944a7e539a7
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2020-02-25 23:19:13 +0000

    Readd test file

---
+++ lib/dialer/empty_test.go
@@ -0,0 +1,10 @@
+// Copyright (C) 2016 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// The existence of this file means we get 0% test coverage rather than no
+// test coverage at all. Remove when implementing an actual test.
+
+package dialer
