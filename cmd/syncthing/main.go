a55188359545270d19f6a96f5aa4b8c6745f122e
Simon Frei <freisim93@gmail.com>
2020-02-27 09:42:47 +0100

    bot

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -66,10 +66,10 @@ above). The value 0 is used to disable all of the above. The default is to
 show time only (2).
 
 Logging always happens to the command line (stdout) and optionally to the
-file at the path specified by -logfile=path. In addition to an explicity path,
-the special values "default" and "-" may be used. The former logs to
-DATADIR/syncthing.log (see -data-dir), which is the default on windows, and the
-latter only to stdout, no file, which is the default anywhere else.
+file at the path specified by -logfile=path. In addition to an path, the special
+values "default" and "-" may be used. The former logs to DATADIR/syncthing.log
+(see -data-dir), which is the default on windows, and the latter only to stdout,
+no file, which is the default anywhere else.
 
 
 Development Settings
