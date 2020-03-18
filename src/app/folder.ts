75b3b31d11e8d19e5c7bbf2202bb7849babdfa94
Jesse Lucas <jesse@jesselucas.com>
2020-03-17 22:36:57 -0400

    create folder status interface

--- src/app/folder.ts
+++ src/app/folder.ts
@@ -1,6 +1,37 @@
 export interface Folder {
     id: string;
     label: string;
+    status: FolderStatus;
+}
 
-    // TODO add additional properties
+export interface FolderStatus {
+    globalBytes: number;
+    globalDeleted: number;
+    globalDirectories: number;
+    globalFiles: number;
+    globalSymlinks: number;
+    ignorePatterns: boolean;
+    inSyncBytes: number;
+    inSyncFiles: number;
+    invalid: string;
+    localBytes: number;
+    localDeleted: number;
+    localDirectories: number;
+    localFiles: number;
+    localSymlinks: number;
+    needBytes: number;
+    needDeletes: number;
+    needDirectories: number;
+    needFiles: number;
+    needSymlinks: number;
+    pullErrors: number;
+    receiveOnlyChangedBytes: number;
+    receiveOnlyChangedDeletes: number;
+    receiveOnlyChangedDirectories: number;
+    receiveOnlyChangedFiles: number;
+    receiveOnlyChangedSymlinks: number;
+    sequence: number;
+    state: string;
+    stateChanged: string;
+    version: number;
 }
