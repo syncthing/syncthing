ee465c089027632c396b3f0d1599c1c6d7408b5b
Jesse Lucas <jesse@jesselucas.com>
2020-04-04 16:13:12 -0400

    update progress service with get percentValue and table tests

--- src/app/services/progress.service.ts
+++ src/app/services/progress.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProgressService {
+  private progress: number = 0;
+  private _total: number = 0;
+  set total(t: number) {
+    this._total = t;
+  }
+
+  get percentValue(): string {
+    let p: number = Math.floor((this.progress / this._total) * 100);
+    console.log("P?!", NaN)
+    if (p < 0 || isNaN(p) || p === Infinity) {
+      p = 0;
+    } else if (p > 100) {
+      p = 100;
+    }
+    return p.toString();
+  }
 
   constructor() { }
+
+  updateProgress(n: number) {
+    if (n < 0) {
+      n = 0
+    } else if (n > 100) {
+      n = 100
+    }
+
+    this.progress = n;
+  }
 }
