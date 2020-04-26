69970db66072170a230160507beffe6d7ce538a1
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2020-04-26 21:33:35 +0100

    Log sequences

--- lib/model/model_test.go
+++ lib/model/model_test.go
@@ -3642,6 +3642,7 @@ func TestRenameSequenceOrder(t *testing.T) {
 	snap = fset.Snapshot()
 	defer snap.Release()
 	snap.WithHaveSequence(0, func(i db.FileIntf) bool {
+		t.Log(i)
 		switch i.SequenceNo() {
 		case 3:
 			assert(t, i.FileName() == "a" && !i.IsDeleted())
