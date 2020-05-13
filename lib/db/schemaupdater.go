974551375eedd53a799c98467a4a5dbd49d158f5
Simon Frei <freisim93@gmail.com>
2020-05-13 20:38:21 +0200

    lib/db: Dont add symlinks to blocks map (fixes #6637) (#6639)

--- lib/db/schemaupdater.go
+++ lib/db/schemaupdater.go
@@ -590,7 +590,7 @@ func (db *schemaUpdater) updateSchemaTo11(_ int) error {
 		var putErr error
 		err := t.withHave(folder, protocol.LocalDeviceID[:], nil, true, func(fi FileIntf) bool {
 			f := fi.(FileInfoTruncated)
-			if f.IsDirectory() || f.IsDeleted() || f.IsInvalid() || f.BlocksHash == nil {
+			if f.IsDirectory() || f.IsDeleted() || f.IsSymlink() || f.IsInvalid() || f.BlocksHash == nil {
 				return true
 			}
 
