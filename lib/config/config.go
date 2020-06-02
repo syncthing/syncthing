68a4aba2a5ce45e215fb96eaa6ee6f283281da01
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2020-06-02 12:43:46 +0100

    Update lib/config/config.go

--- lib/config/config.go
+++ lib/config/config.go
@@ -133,7 +133,7 @@ func NewWithFreePorts(myID protocol.DeviceID) (Configuration, error) {
 		cfg.Options.RawListenAddresses = []string{"default"}
 	} else {
 		cfg.Options.RawListenAddresses = []string{
-			fmt.Sprintf("tcp://%s", net.JoinHostPort("0.0.0.0", strconv.Itoa(port))),
+			util.Address("tcp", net.JoinHostPort("0.0.0.0", strconv.Itoa(port))),
 			"dynamic+https://relays.syncthing.net/endpoint",
 			util.Address("quic", net.JoinHostPort("0.0.0.0", strconv.Itoa(port))),
 		}
