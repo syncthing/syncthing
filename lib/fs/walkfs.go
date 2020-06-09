56def11e387504346ef5c5425ef028be7fcd5390
Simon Frei <freisim93@gmail.com>
2020-06-09 23:21:19 +0200

    lib: Walk fs and db simultaneously on scan

--- lib/fs/walkfs.go
+++ lib/fs/walkfs.go
@@ -12,6 +12,7 @@ package fs
 
 import (
 	"path/filepath"
+	"sort"
 )
 
 type ancestorDirList struct {
@@ -98,6 +99,7 @@ func (f *walkFilesystem) walk(path string, info FileInfo, walkFn WalkFunc, ances
 	if err != nil {
 		return walkFn(path, info, err)
 	}
+	sort.Strings(names)
 
 	for _, name := range names {
 		filename := filepath.Join(path, name)
