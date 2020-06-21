7f9140ecdb612d5159db3d69dee4febcd3dc4b7a
Simon Frei <freisim93@gmail.com>
2020-06-21 14:52:37 +0200

    lib/api, lib/model: Fixes around event request tracking

--- lib/api/api.go
+++ lib/api/api.go
@@ -1202,7 +1202,6 @@ func (s *service) postDBIgnores(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *service) getIndexEvents(w http.ResponseWriter, r *http.Request) {
-	s.fss.OnEventRequest()
 	mask := s.getEventMask(r.URL.Query().Get("events"))
 	sub := s.getEventSub(mask)
 	s.getEvents(w, r, sub)
@@ -1214,6 +1213,8 @@ func (s *service) getDiskEvents(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *service) getEvents(w http.ResponseWriter, r *http.Request, eventSub events.BufferedSubscription) {
+	s.fss.OnEventRequest()
+
 	qs := r.URL.Query()
 	sinceStr := qs.Get("since")
 	limitStr := qs.Get("limit")
