ae208df40a1952f0bd3c28be2a5c27c6a1355008
Audrius Butkevicius <audrius.butkevicius@gmail.com>
2020-08-18 20:07:23 +0100

    Update basicfs_copy_range_ioctl.go

--- lib/fs/basicfs_copy_range_ioctl.go
+++ lib/fs/basicfs_copy_range_ioctl.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// +build linux,!ppc,!ppc64,!ppc64le 
+// +build linux,!ppc,!ppc64,!ppc64le
 
 package fs
 
