c7a0707a0820f211682326783d55bb510db157b5
Jakob Borg <jakob@kastelo.net>
2020-09-28 10:13:12 +0200

    wip

--- lib/fs/basicfs.go
+++ lib/fs/basicfs.go
@@ -22,7 +22,7 @@ var (
 	errInvalidFilenameEmpty               = errors.New("name is invalid, must not be empty")
 	errInvalidFilenameWindowsSpacePeriod  = errors.New("name is invalid, must not end in space or period on Windows")
 	errInvalidFilenameWindowsReservedName = errors.New("name is invalid, contains Windows reserved name (NUL, COM1, etc.)")
-	errInvalidFilenameWindowsReservedChar = errors.New("name is invalid, contains Windows reserved character (?, *, :, etc.)")
+	errInvalidFilenameWindowsReservedChar = errors.New("name is invalid, contains Windows reserved character (?, *, etc.)")
 	errNotRelative                        = errors.New("not a relative path")
 )
 
