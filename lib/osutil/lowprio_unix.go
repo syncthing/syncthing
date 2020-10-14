6429b043428733bf5311b3504ad8a5b9d572abd6
Simon Pickup <simon@pickupinfinity.com>
2020-10-14 13:50:12 +0100

    Disable SetLowPriority on ios

--- lib/osutil/lowprio_unix.go
+++ lib/osutil/lowprio_unix.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// +build !windows,!linux android
+// +build !windows,!linux,!ios android
 
 package osutil
 
