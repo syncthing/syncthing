8cb0e6e078880ded144afe64d7c69a168e455c60
André Colomb <src@andre.colomb.de>
2020-10-19 00:00:36 +0200

    lib/db: Resurrect PendingFolders() and PendingDevices().
    This reimplements the functions removed in commit
    778cb59b4d5ceb2191d8852ae714325d86ae8b2f, except for a simpler return
    interface.  Both functions now pass out simple slices of a suitable
    structure type which contains the identifying fields from the DB key
    alongside the stored Observed... structure (embedded).

--- lib/db/observed.go
+++ lib/db/observed.go
@@ -34,6 +34,49 @@ func (db *Lowlevel) RemovePendingDevice(device protocol.DeviceID) {
 	}
 }
 
+// PendingDevice combines the DB key and value contents for passing around
+type PendingDevice struct {
+	DeviceID protocol.DeviceID
+	ObservedDevice
+}
+
+// PendingDevices drops any invalid entries from the database after a
+// warning log message, as a side-effect.  That's the only possible
+// "repair" measure and appropriate for the importance of pending
+// entries.  They will come back soon if still relevant.
+func (db *Lowlevel) PendingDevices() ([]PendingDevice, error) {
+	iter, err := db.NewPrefixIterator([]byte{KeyTypePendingDevice})
+	if err != nil {
+		l.Infof("Could not iterate through pending device entries: %v", err)
+		return nil, err
+	}
+	defer iter.Release()
+	var res []PendingDevice
+	for iter.Next() {
+		keyDev := db.keyer.DeviceFromPendingDeviceKey(iter.Key())
+		deviceID, err := protocol.DeviceIDFromBytes(keyDev)
+		var bs []byte
+		var od ObservedDevice
+		if err != nil {
+			goto deleteKey
+		}
+		if bs, err = db.Get(iter.Key()); err != nil {
+			goto deleteKey
+		}
+		if err = od.Unmarshal(bs); err != nil {
+			goto deleteKey
+		}
+		res = append(res, PendingDevice{deviceID, od})
+		continue
+	deleteKey:
+		l.Infof("Invalid pending device entry, deleting from database: %x", iter.Key())
+		if err := db.Delete(iter.Key()); err != nil {
+			return nil, err
+		}
+	}
+	return res, nil
+}
+
 func (db *Lowlevel) AddOrUpdatePendingFolder(id, label string, device protocol.DeviceID) error {
 	key, err := db.keyer.GeneratePendingFolderKey(nil, device[:], []byte(id))
 	if err != nil {
@@ -59,6 +102,51 @@ func (db *Lowlevel) RemovePendingFolder(id string, device protocol.DeviceID) {
 		}
 	}
 	l.Warnf("Failed to remove pending folder entry: %v", err)
+}
+
+// PendingFolder combines the DB key and value contents for passing around
+type PendingFolder struct {
+	FolderID string
+	DeviceID protocol.DeviceID
+	ObservedFolder
+}
+
+// PendingFolders drops any invalid entries from the database as a side-effect.
+func (db *Lowlevel) PendingFolders() ([]PendingFolder, error) {
+	iter, err := db.NewPrefixIterator([]byte{KeyTypePendingFolder})
+	if err != nil {
+		l.Infof("Could not iterate through pending folder entries: %v", err)
+		return nil, err
+	}
+	defer iter.Release()
+	var res []PendingFolder
+	for iter.Next() {
+		keyDev, ok := db.keyer.DeviceFromPendingFolderKey(iter.Key())
+		deviceID, err := protocol.DeviceIDFromBytes(keyDev)
+		var of ObservedFolder
+		var folderID string
+		var bs []byte
+		if !ok || err != nil {
+			goto deleteKey
+		}
+		if folderID = string(db.keyer.FolderFromPendingFolderKey(iter.Key())); len(folderID) < 1 {
+			goto deleteKey
+		}
+		if bs, err = db.Get(iter.Key()); err != nil {
+			goto deleteKey
+		}
+		if err = of.Unmarshal(bs); err != nil {
+			goto deleteKey
+		}
+		res = append(res, PendingFolder{folderID, deviceID, of})
+		continue
+	deleteKey:
+		l.Infof("Invalid pending folder entry, deleting from database: %x", iter.Key())
+		if err := db.Delete(iter.Key()); err != nil {
+			return nil, err
+		}
+	}
+	return res, nil
 }
 
 // PendingDeviceIterator abstracts away the key handling and validation, yielding only
