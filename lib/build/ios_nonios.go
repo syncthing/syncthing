a99e2d11917a27486cb79878a8e74e659cd37f14
Simon Pickup <simon@pickupinfinity.com>
2020-11-10 17:36:40 +0000

    Determine IsIOS based on build tag

--- lib/build/ios_nonios.go
+++ lib/build/ios_nonios.go
@@ -4,12 +4,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// +build !ios
+
 package build
 
-import (
-	"runtime"
-)
-
 func IsIOS() bool {
-	return (runtime.GOOS == "darwin" && runtime.GOARCH == "arm64")
+	return false
 }
