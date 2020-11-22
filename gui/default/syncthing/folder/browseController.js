21b8aabfa8e75a5dee48404dee228de7bbd83aab
Christian Prescott <me@christianprescott.com>
2020-11-22 13:47:26 -0700

    gui: Add ignore pattern matching to browser

--- gui/default/syncthing/folder/browseController.js
+++ gui/default/syncthing/folder/browseController.js
@@ -14,6 +14,17 @@ angular.module('syncthing.folder')
             list: [],
             */
         };
+        // Parsed ignore pattern objects. Order matters, first match is applied.
+        self.ignorePatterns = [
+            /*
+            {
+                text: '/Photos', // original text of the ignore pattern
+                isSimple: true, // whether the pattern is unambiguous and can be displayed by browser
+                isNegated: false, // begins with !, matching files are included
+                path: '/Photos', // path to the file or directory, stripped of prefix and used for matching
+            }
+            */
+        ];
         self.folder = CurrentFolder;
 
         $scope.$watch(function() {
@@ -25,8 +36,12 @@ angular.module('syncthing.folder')
         });
 
         function refresh(folderId) {
-            getBrowse(self.folder.id).then(function (response) {
-                self.browse = response;
+            $q.all([
+                getIgnores(self.folder.id),
+                getBrowse(self.folder.id),
+            ]).then(function (responses) {
+                self.ignorePatterns = responses[0];
+                self.browse = responses[1];
             });
         };
 
@@ -79,14 +94,9 @@ angular.module('syncthing.folder')
             var items = [];
             for (var name in data) {
                 var isFile = Array.isArray(data[name]);
-                var path = pathPrefix.concat([name]);
-                if (!isFile) {
-                    // Add a trailing slash to directory path to easily match pattern path
-                    path.push('');
-                }
                 var item = {
                     name: name,
-                    path: path.join('/'),
+                    path: pathPrefix.concat([name]).join('/'),
                     isFile: isFile
                 };
                 if (isFile) {
@@ -97,4 +107,92 @@ angular.module('syncthing.folder')
             }
             return items;
         };
+
+        /*
+         * Ignore Patterns
+         */
+
+        function getIgnores(folderId) {
+            return $http.get('rest/db/ignores', {
+                params: { folder: folderId }
+            }).then(function (response) {
+                var data = response.data;
+                return data.ignore.map(parsePattern);
+            });
+        }
+
+        function parsePattern(line) {
+            var stripResult = stripPrefix(line.trim());
+            var prefixes = stripResult[0];
+            var hasPrefix = prefixes['(?i)'] || prefixes['(?d)'];
+            var path = toPath(stripResult[1]);
+
+            return {
+                text: line,
+                isSimple: !hasPrefix && isSimple(path),
+                isNegated: prefixes['!'],
+                path: path,
+            };
+        };
+
+        // Adapted from lib/ignore/ignore.go@parseLine
+        function stripPrefix(line) {
+            var seenPrefix = { '!': false, '(?i)': false, '(?d)': false };
+
+            var found;
+            while (found !== null) {
+                found = null;
+                for (var prefix in seenPrefix) {
+                    if (line.indexOf(prefix) === 0 && !seenPrefix[prefix]) {
+                        seenPrefix[prefix] = true;
+                        line = line.slice(prefix.length);
+                        found = prefix;
+                        break;
+                    }
+                }
+            }
+
+            return [seenPrefix, line];
+        }
+
+        // Infer a path from wildcards that can be used to match a file by
+        // prefix (for simple patterns)
+        function toPath(line) {
+            line = line.replace(/^\*+/, '/$&');
+            // Trim wildcards after final separator for simpler path match
+            line = line.replace(/\/\*+$/, '/');
+            return line;
+        }
+
+        // A "simple" pattern is one that is anchored at folder root and can be
+        // displayed by our browser.
+        function isSimple(line) {
+            if (line.indexOf('/') !== 0) return false; // not a root line
+            if (line.indexOf('//') === 0) return false; // comment
+            if (line.length > 1 && line.charAt(line.length - 1) === '/') return false; // trailing slash
+
+            line = line.replaceAll(/\\[\*\?\[\]\{\}]/g, '') // remove properly escaped characters for this evaluation
+            if (line.match(/[\*\?\[\]\{\}]/)) return false; // contains special character
+
+            return true;
+        }
+
+        self.matchingPatterns = function(file) {
+            return self.ignorePatterns.filter(function(pattern) {
+                // Only consider patterns that match a simple path
+                if (!pattern.isSimple) return false;
+
+                var absPath = '/' + file.path;
+                if (absPath.indexOf(pattern.path) !== 0) return false;
+
+                // pattern ends with path separator, file is a child of the pattern path
+                if (pattern.path.charAt(pattern.path.length - 1) === '/') return true;
+
+                var suffix = absPath.slice(pattern.path.length);
+                // pattern is an exact match to file path
+                if (suffix.length === 0) return true;
+                // pattern is an exact match to a parent directory in the file path
+                return suffix.charAt(0) === '/';
+            });
+        }
     });
--- gui/default/syncthing/folder/browseController.test.js
+++ gui/default/syncthing/folder/browseController.test.js
@@ -2,12 +2,15 @@ describe('BrowseController', function() {
     // Set up the module
     beforeEach(module('syncthing.folder'));
 
-    var $controller, $scope, $httpBackend, getBrowseHandler;
+    var $controller, $scope, $httpBackend, getIgnoresHandler, getBrowseHandler;
     var controller;
 
     // Inject angular bits
     beforeEach(inject(function($injector) {
         $httpBackend = $injector.get('$httpBackend');
+        // Common controller init requests
+        // Tests override these responses where necessary
+        getIgnoresHandler = $httpBackend.when('GET', /^rest\/db\/ignores/).respond({ ignore: [] });
         getBrowseHandler = $httpBackend.when('GET', /^rest\/db\/browse/).respond({});
 
         $controller = $injector.get('$controller');
@@ -27,6 +30,7 @@ describe('BrowseController', function() {
         });
 
         it('fetches data when initialized', function() {
+            $httpBackend.expectGET('rest/db/ignores?folder=default');
             $httpBackend.expectGET('rest/db/browse?folder=default&levels=0');
             controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
             $httpBackend.flush();
@@ -34,12 +38,162 @@ describe('BrowseController', function() {
 
         it('fetches data when current folder changes', function() {
             var folder = { id: 'default' };
+            $httpBackend.expectGET('rest/db/ignores?folder=default');
             $httpBackend.expectGET('rest/db/browse?folder=default&levels=0');
             controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: folder });
             $httpBackend.flush();
             folder.id = 'documents';
+            $httpBackend.expectGET('rest/db/ignores?folder=documents');
             $httpBackend.expectGET('rest/db/browse?folder=documents&levels=0');
             $httpBackend.flush();
+        });
+    });
+
+    describe('ignores', function() {
+        function mockIgnores(ignores) {
+            getIgnoresHandler.respond({ ignore: ignores });
+            // Initialize the controller, triggering http gets and populating data
+            controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
+            $httpBackend.flush();
+        }
+
+        it('returns a pattern for each line', function() {
+            mockIgnores([
+                '/Backups',
+                'Photos/**/Raw'
+            ]);
+            expect(controller.ignorePatterns.length).toEqual(2);
+        });
+
+        it('identifies negated patterns', function() {
+            mockIgnores(['!/Backups', '(?i)!/Photos/Raw']);
+            expect(controller.ignorePatterns.every(function (p) { return p.isNegated })).toBeTrue();
+
+            mockIgnores(['/IMPORTANT FILES!', '(?i)/!Photos/Raw']);
+            expect(controller.ignorePatterns.every(function (p) { return !p.isNegated })).toBeTrue();
+        });
+
+        describe('identifies simple patterns', function() {
+            function isSimple(p) { return p.isSimple; };
+
+            it('by root folder', function() {
+                mockIgnores(['/Backups', ' /Photos/Raw', '/']);
+                expect(controller.ignorePatterns.every(isSimple)).toBeTrue();
+            });
+
+            it('by root wildcard', function() {
+                mockIgnores(['*', '**', '/*']);
+                expect(controller.ignorePatterns.every(isSimple)).toBeTrue();
+            });
+
+            it('when negated', function() {
+                mockIgnores(['!/Backups']);
+                expect(controller.ignorePatterns.every(isSimple)).toBeTrue();
+            });
+
+            it('with escaped special characters', function() {
+                mockIgnores(['/IMPORTANT\\*\\*FILES', '/mustache pics :\\{', '/Square\\[shaped\\]Images']);
+                expect(controller.ignorePatterns.every(isSimple)).toBeTrue();
+            });
+        });
+
+        describe('identifies advanced patterns', function() {
+            function isAdvanced(p) { return !p.isSimple; };
+
+            it('missing root path', function() {
+                mockIgnores(['Backups']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('with subdirectory', function() {
+                mockIgnores(['Backups/June', '!Photos/Raw']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            // This pattern looks simple, but differs in behavior:
+            // /backups ignores the directory
+            // /backups/ or /backups/** includes the directory but ignores the contents
+            // The latter is tricky for the browser, so it is "advanced" for now.
+            it('with trailing slash', function() {
+                mockIgnores(['/Backups/', '/Backups/**']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by empty line', function() {
+                mockIgnores(['', '(?i)']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by (?d)(?i) prefixes', function() {
+                mockIgnores(['(?i)/Backups', '(?d)/Backups']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by * wildcard', function() {
+                mockIgnores(['/Back*ups', '/Backups*', '/Photos/**/Raw', '**/Photos']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by // comment', function() {
+                mockIgnores(['// Backups are too big']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by ? character', function() {
+                mockIgnores(['/Backup?/June']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by [] character range', function() {
+                mockIgnores(['/Backups/June200[0-9]']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+
+            it('by {} alternatives', function() {
+                mockIgnores(['/Backups/{June,July}2009']);
+                expect(controller.ignorePatterns.every(isAdvanced)).toBeTrue();
+            });
+        });
+
+        describe('path', function() {
+            it('returns root path', function() {
+                mockIgnores(['*', '**', '/', '/*']);
+                expect(controller.ignorePatterns[0].path).toEqual('/');
+                expect(controller.ignorePatterns[1].path).toEqual('/');
+                expect(controller.ignorePatterns[2].path).toEqual('/');
+                expect(controller.ignorePatterns[3].path).toEqual('/');
+            });
+
+            it('returns simple file path', function() {
+                mockIgnores(['/Backups', '/Backups/', ' /Photos/Raw']);
+                expect(controller.ignorePatterns[0].path).toEqual('/Backups');
+                expect(controller.ignorePatterns[1].path).toEqual('/Backups/');
+                expect(controller.ignorePatterns[2].path).toEqual('/Photos/Raw');
+            });
+
+            it('strips prefixes', function() {
+                mockIgnores(['!/Backups', '(?i)!/!Photos/Raw']);
+                expect(controller.ignorePatterns[0].path).toEqual('/Backups');
+                expect(controller.ignorePatterns[1].path).toEqual('/!Photos/Raw');
+            });
+
+            it('strips trailing wildcard after path separator', function() {
+                mockIgnores(['/Backups*', '!/Photos/Raw/**']);
+                expect(controller.ignorePatterns[0].path).toEqual('/Backups*');
+                expect(controller.ignorePatterns[1].path).toEqual('/Photos/Raw/');
+            });
+
+            it('does not strip leading wildcard', function() {
+                mockIgnores(['**Backups', '**/Photos']);
+                expect(controller.ignorePatterns[0].path).toEqual('/**Backups');
+                expect(controller.ignorePatterns[1].path).toEqual('/**/Photos');
+            });
+
+            it('does not add leading slash to bare name', function() {
+                mockIgnores(['Backups/', ' Photos/Raw']);
+                expect(controller.ignorePatterns[0].path).toEqual('Backups/');
+                expect(controller.ignorePatterns[1].path).toEqual('Photos/Raw');
+            });
         });
     });
 
@@ -102,8 +256,8 @@ describe('BrowseController', function() {
                 expect(controller.browse.list[0].path).toEqual('homework.txt');
             });
 
-            it('populates directory path with trailing slash', function() {
-                expect(controller.browse.list[1].path).toEqual('Photos/');
+            it('populates directory path', function() {
+                expect(controller.browse.list[1].path).toEqual('Photos');
             });
 
             it('populates path with parent directory', function() {
@@ -114,7 +268,7 @@ describe('BrowseController', function() {
                 controller.navigate('default', 'Photos');
                 $httpBackend.flush();
                 expect(controller.browse.list[0].path).toEqual('Photos/image.jpg');
-                expect(controller.browse.list[1].path).toEqual('Photos/Raw/');
+                expect(controller.browse.list[1].path).toEqual('Photos/Raw');
             });
 
             it('does not duplicate slash in prefix', function() {
@@ -125,7 +279,7 @@ describe('BrowseController', function() {
                 controller.navigate('default', 'Photos/');
                 $httpBackend.flush();
                 expect(controller.browse.list[0].path).toEqual('Photos/image.jpg');
-                expect(controller.browse.list[1].path).toEqual('Photos/Raw/');
+                expect(controller.browse.list[1].path).toEqual('Photos/Raw');
             });
         });
 
@@ -156,6 +310,167 @@ describe('BrowseController', function() {
                     { name: 'Photos', prefix: 'Photos' },
                     { name: 'Raw', prefix: 'Photos/Raw' },
                 ]);
+            });
+        });
+    });
+
+    describe('matchingPatterns', function() {
+        beforeEach(function() {
+            getBrowseHandler.respond({
+                'Backups.zip': [],
+                Backups: {},
+                Documents: {},
+                'Photostudio.exe': [],
+                Photos: {},
+            });
+        });
+
+        function findFile(name) {
+            var file = controller.browse.list.find(function(f) { return f.name === name; })
+            if (!file) {
+                throw 'No file with name "' + name + '" in scope';
+            }
+            return file;
+        }
+
+        it('applies the first matching rule', function() {
+            getIgnoresHandler.respond({ ignore: [
+                '/Backups',
+                '/Photos',
+                '!/Photos',
+                '*',
+            ] });
+            controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
+            $httpBackend.flush();
+            var matches = controller.matchingPatterns(findFile('Photos'));
+            expect(matches.length).toEqual(3);
+            expect(matches[0].text).toEqual('/Photos');
+        });
+
+        it('does not match advanced patterns', function() {
+            getIgnoresHandler.respond({ ignore: [
+                '/Backup?',
+                '/Backup*',
+                'Backups',
+            ] });
+            controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
+            $httpBackend.flush();
+            var matches = controller.matchingPatterns(findFile('Backups'));
+            expect(matches.length).toEqual(0);
+        });
+
+        describe('when directories are ignored', function() {
+            beforeEach(function() {
+                getIgnoresHandler.respond({ ignore: [
+                    '/Backups.zip/',
+                    '/Backups',
+                    '/Documents/',
+                    '!/Photos/Raw',
+                    '/Photos',
+                    '/Photostudio.exe',
+                ] });
+                controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
+                $httpBackend.flush();
+            });
+
+            it('matches files', function() {
+                var matches = controller.matchingPatterns(findFile('Photostudio.exe'));
+                expect(matches.length).toEqual(1);
+                expect(matches[0].text).toEqual('/Photostudio.exe');
+            });
+
+            it('does not match files to pattern with trailing slash', function() {
+                var matches = controller.matchingPatterns(findFile('Backups.zip'));
+                expect(matches.length).toEqual(0);
+            });
+
+            it('matches directories', function() {
+                var matches = controller.matchingPatterns(findFile('Backups'));
+                expect(matches.length).toEqual(1);
+                expect(matches[0].text).toEqual('/Backups');
+            });
+
+            it('does not match directories to pattern with trailing slash', function() {
+                var matches = controller.matchingPatterns(findFile('Documents'));
+                expect(matches.length).toEqual(0);
+            });
+
+            it('matches directory with more specific negated pattern', function() {
+                var matches = controller.matchingPatterns(findFile('Photos'));
+                expect(matches.length).toEqual(1);
+                expect(matches[0].text).toEqual('/Photos');
+                expect(matches[0].isNegated).toBeFalse();
+            });
+
+            describe('in subdirectory', function() {
+                beforeEach(function() {
+                    // Same ignore patterns, but viewing the "Backups" directory
+                    getBrowseHandler.respond({
+                        June2008: {},
+                    });
+                    controller.navigate('default', 'Backups');
+                    $httpBackend.flush();
+                });
+
+                it('matches by parent directory', function() {
+                    var matches = controller.matchingPatterns(findFile('June2008'));
+                    expect(matches.length).toEqual(1);
+                    expect(matches[0].text).toEqual('/Backups');
+                });
+            });
+
+            describe('in ignored subdirectory', function() {
+                beforeEach(function() {
+                    getBrowseHandler.respond({
+                        'Cat.jpg': [],
+                        'Rawr.jpg': [],
+                        Raw: {},
+                    });
+                    controller.navigate('default', 'Photos');
+                    $httpBackend.flush();
+                });
+
+                it('matches ignored files', function() {
+                    var matches = controller.matchingPatterns(findFile('Cat.jpg'));
+                    expect(matches.length).toEqual(1);
+                    expect(matches[0].text).toEqual('/Photos');
+                });
+
+                it('does not negate files with common prefix', function() {
+                    var matches = controller.matchingPatterns(findFile('Rawr.jpg'));
+                    expect(matches.length).toEqual(1);
+                    expect(matches[0].text).toEqual('/Photos');
+                });
+
+                it('matches negated subdirectory with more specific pattern', function() {
+                    var matches = controller.matchingPatterns(findFile('Raw'));
+                    expect(matches.length).toEqual(2);
+                    expect(matches[0].text).toEqual('!/Photos/Raw');
+                });
+            });
+        });
+
+        describe('with root ignored', function() {
+            beforeEach(function() {
+                getIgnoresHandler.respond({ ignore: [
+                    '!/Photos',
+                    '*',
+                ] });
+                controller = $controller('BrowseController', { $scope: $scope, CurrentFolder: { id: 'default' } });
+                $httpBackend.flush();
+            });
+
+            it('matches root files and directories', function() {
+                ['Backups.zip', 'Backups'].forEach(function(file) {
+                    var matches = controller.matchingPatterns(findFile(file));
+                    expect(matches.length).toEqual(1);
+                });
+            });
+
+            it('matches negated directories', function() {
+                var matches = controller.matchingPatterns(findFile('Photos'));
+                expect(matches.length).toEqual(2);
+                expect(matches[0].isNegated).toBeTrue();
             });
         });
     });
