765878e80ec6005cae9251d7adc5575bd110878f
Jakob Borg <jakob@kastelo.net>
2020-12-08 18:12:32 +0100

    go1.14

--- lib/connections/quic_dial.go
+++ lib/connections/quic_dial.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// +build go1.12,!noquic,!go1.16
+// +build go1.14,!noquic,!go1.16
 
 package connections
 
