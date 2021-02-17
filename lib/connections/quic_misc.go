327604719a6da437fcfa9a8cb1d2f09b2994c6df
Jakob Borg <jakob@kastelo.net>
2021-02-17 11:09:16 +0100

    lib/connections: Allow QUIC with Go 1.16 (#7372)

--- lib/connections/quic_misc.go
+++ lib/connections/quic_misc.go
@@ -4,7 +4,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// +build go1.14,!noquic,!go1.16
+// +build go1.14,!noquic,!go1.17
 
 package connections
 
