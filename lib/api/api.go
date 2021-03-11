8e19e86d1e6796d73af109a16294408df1ef8d49
Simon Frei <freisim93@gmail.com>
2021-03-11 15:20:56 +0100

    lib/api: Fix body of renamed config/restart-required endpoint (ref #7402)

--- lib/api/api.go
+++ lib/api/api.go
@@ -298,7 +298,7 @@ func (s *service) Serve(ctx context.Context) error {
 
 	configBuilder.registerConfig("/rest/config")
 	configBuilder.registerConfigInsync("/rest/config/insync") // deprecated
-	configBuilder.registerConfigInsync("/rest/config/restart-required")
+	configBuilder.registerConfigRequiresRestart("/rest/config/restart-required")
 	configBuilder.registerFolders("/rest/config/folders")
 	configBuilder.registerDevices("/rest/config/devices")
 	configBuilder.registerFolder("/rest/config/folders/:id")
