syncthing/syncthing

commit 4d979a1ce994d557a1a132f6a816715dd52b0572

all: Truncate some timestamps (fixes #7457) (#7459)
This truncates times meant for API consumption to second precision,
where fractions won't typically matter or add any value. Exception to
this is timestamps on logs and events, and of course I'm not touching
things like file metadata.

I'm not 100% certain this is an exhaustive change, but it's the things I
found by grepping and following the breadcrumbs from lib/api...

I also considered general-but-ugly solutions, like having the API
serializer itself do reflection magic or even regexps on returned
objects, but decided against it because aurgh...

lib/db/observed.go
@@ -15,7 +15,7 @@ import (
 func (db *Lowlevel) AddOrUpdatePendingDevice(device protocol.DeviceID, name, address string) error {
 	key := db.keyer.GeneratePendingDeviceKey(nil, device[:])
 	od := ObservedDevice{
-		Time:    time.Now().Round(time.Second),
+		Time:    time.Now().Truncate(time.Second),
 		Name:    name,
 		Address: address,
 	}
@@ -72,7 +72,7 @@ func (db *Lowlevel) AddOrUpdatePendingFolder(id, label string, device protocol.D
 		return err
 	}
 	of := ObservedFolder{
-		Time:             time.Now().Round(time.Second),
+		Time:             time.Now().Truncate(time.Second),
 		Label:            label,
 		ReceiveEncrypted: receiveEncrypted,
 	}

cmd/strelaypoolsrv/main.go
@@ -10,7 +10,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/syncthing/syncthing/lib/protocol"
 	"io"
 	"io/ioutil"
 	"log"
@@ -22,6 +21,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/syncthing/syncthing/lib/protocol"
 
 	"github.com/golang/groupcache/lru"
 	"github.com/oschwald/geoip2-golang"
@@ -475,7 +476,7 @@ func handleRelayTest(request request) {
 		updateMetrics(request.relay.uri.Host, *stats, location)
 	}
 	request.relay.Stats = stats
-	request.relay.StatsRetrieved = time.Now()
+	request.relay.StatsRetrieved = time.Now().Truncate(time.Second)
 	request.relay.Location = location
 
 	timer, ok := evictionTimers[request.relay.uri.Host]

lib/connections/structs.go
@@ -89,7 +89,7 @@ func newInternalConn(tc tlsConn, connType connType, priority int) internalConn {
 		tlsConn:       tc,
 		connType:      connType,
 		priority:      priority,
-		establishedAt: time.Now(),
+		establishedAt: time.Now().Truncate(time.Second),
 	}
 }
 

lib/events/events.go
@@ -317,7 +317,7 @@ loop:
 
 func (l *logger) Log(t EventType, data interface{}) {
 	l.events <- Event{
-		Time: time.Now(),
+		Time: time.Now(), // intentionally high precision
 		Type: t,
 		Data: data,
 		// SubscriptionID and GlobalID are set in sendEvent

lib/logger/logger.go
@@ -337,7 +337,7 @@ func (r *recorder) Clear() {
 
 func (r *recorder) append(l LogLevel, msg string) {
 	line := Line{
-		When:    time.Now(),
+		When:    time.Now(), // intentionally high precision
 		Message: msg,
 		Level:   l,
 	}

lib/model/folderstate.go
@@ -100,7 +100,7 @@ func (s *stateTracker) setState(newState folderState) {
 	}
 
 	s.current = newState
-	s.changed = time.Now()
+	s.changed = time.Now().Truncate(time.Second)
 
 	s.evLogger.Log(events.StateChanged, eventData)
 }
@@ -139,7 +139,7 @@ func (s *stateTracker) setError(err error) {
 	}
 
 	s.err = err
-	s.changed = time.Now()
+	s.changed = time.Now().Truncate(time.Second)
 
 	s.evLogger.Log(events.StateChanged, eventData)
 }

lib/model/model.go
@@ -769,7 +769,7 @@ func (m *model) ConnectionStats() map[string]interface{} {
 	in, out := protocol.TotalInOut()
 	res["total"] = ConnectionInfo{
 		Statistics: protocol.Statistics{
-			At:            time.Now(),
+			At:            time.Now().Truncate(time.Second),
 			InBytesTotal:  in,
 			OutBytesTotal: out,
 		},

lib/protocol/protocol.go
@@ -296,7 +296,7 @@ func (c *rawConnection) Start() {
 		c.pingReceiver()
 		c.loopWG.Done()
 	}()
-	c.startTime = time.Now()
+	c.startTime = time.Now().Truncate(time.Second)
 }
 
 func (c *rawConnection) ID() DeviceID {
@@ -1040,7 +1040,7 @@ type Statistics struct {
 
 func (c *rawConnection) Statistics() Statistics {
 	return Statistics{
-		At:            time.Now(),
+		At:            time.Now().Truncate(time.Second),
 		InBytesTotal:  c.cr.Tot(),
 		OutBytesTotal: c.cw.Tot(),
 		StartedAt:     c.startTime,

commit 395206c7acfd0848710b59b35320a1ad94dea1f5

lib/db: Fix comparison of pending folder timestamps (fixes #7532)
Regression introduced in #7459, this one instance of Round() was
overlooked and not changed to Truncate().

lib/db/observed.go
@@ -124,7 +124,7 @@ func (db *Lowlevel) RemovePendingFoldersBeforeTime(device protocol.DeviceID, old
 		return nil, err
 	}
 	defer iter.Release()
-	oldest = oldest.Round(time.Second)
+	oldest = oldest.Truncate(time.Second)
 	var res []string
 	for iter.Next() {
 		var of ObservedFolder