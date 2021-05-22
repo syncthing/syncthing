f9b24eaf23f5042d778fbcba0610ec2b3448eab3
Simon Frei <freisim93@gmail.com>
2021-05-22 22:14:38 +0200

    review

--- gui/default/syncthing/core/syncthingController.js
+++ gui/default/syncthing/core/syncthingController.js
@@ -2117,7 +2117,7 @@ angular.module('syncthing.core')
                 folderCfg.versioning.params.command = '' + folderCfg._guiVersioning.externalCommand;
                 break;
             default:
-                folderCfg.versioning.type = '';
+                folderCfg.versioning = {type: ''};
             }
             delete folderCfg._guiVersioning;
 
