aa5108830c7912b88791f186ae3dc479e84793c6
Naïm Favier <n@monade.li>
2021-07-27 16:30:45 +0200

    lib/syncthing: Remove the warning when insecureAdminAccess is enabled

--- lib/syncthing/syncthing.go
+++ lib/syncthing/syncthing.go
@@ -414,10 +414,6 @@ func (a *App) setupGUI(m model.Model, defaultSub, diskSub events.BufferedSubscri
 		return nil
 	}
 
-	if guiCfg.InsecureAdminAccess {
-		l.Warnln("Insecure admin access is enabled.")
-	}
-
 	summaryService := model.NewFolderSummaryService(a.cfg, m, a.myID, a.evLogger)
 	a.mainService.Add(summaryService)
 
