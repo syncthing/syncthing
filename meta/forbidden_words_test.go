1136eb8a80b0a0fe7918885d607d547d1c7fcad6
Jakob Borg <jakob@kastelo.net>
2021-11-22 09:18:48 +0100

    meta: Add test for deprecated package ioutil

---
+++ meta/forbidden_words_test.go
@@ -0,0 +1,52 @@
+// Copyright (C) 2021 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+// Checks for authors that are not mentioned in AUTHORS
+package meta
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestForbiddenWords(t *testing.T) {
+	checkDirs := []string{"../cmd", "../lib", "../test", "../script"}
+	forbiddenWords := []string{
+		`"io/ioutil"`, // deprecated and should not be imported
+	}
+
+	for _, dir := range checkDirs {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if path == ".git" {
+				return filepath.SkipDir
+			}
+			if filepath.Ext(path) != ".go" || strings.HasSuffix(path, ".pb.go") {
+				return nil
+			}
+
+			bs, err := os.ReadFile(path)
+			if err != nil {
+				return nil
+			}
+
+			for _, word := range forbiddenWords {
+				if bytes.Contains(bs, []byte(word)) {
+					t.Errorf("%s: forbidden word %q", path, word)
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
