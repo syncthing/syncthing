3bfd41c48b5b4cc86aa580abb7a956c149be1d34
Syncthing Automation <automation@syncthing.net>
2022-01-10 13:48:17 +0000

    gui, man, authors: Update docs, translations, and contributors

--- gui/default/assets/lang/valid-langs.js
+++ gui/default/assets/lang/valid-langs.js
@@ -1,1 +1,1 @@
-var validLangs = ["bg","ca@valencia","cs","da","de","el","en","en-AU","en-GB","eo","es","es-ES","eu","fi","fr","fy","hu","id","it","ja","ko-KR","lt","nb","nl","pl","pt-BR","pt-PT","ro-RO","ru","sk","sv","tr","uk","zh-CN","zh-HK","zh-TW"]
+var validLangs = ["bg","ca@valencia","cs","da","de","el","en","en-AU","en-GB","eo","es","es-ES","eu","fi","fr","fy","hu","id","it","ja","ko-KR","lt","nb","nl","pl","pt-BR","pt-PT","ro-RO","ru","sk","sl","sv","tr","uk","zh-CN","zh-HK","zh-TW"]
