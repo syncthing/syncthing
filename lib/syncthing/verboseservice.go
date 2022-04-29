d58fa0ea09c65c12ea01c11da8d6069e18c869ce
André Colomb <src@andre.colomb.de>
2022-04-29 22:51:56 +0200

    lib/syncthing: Fix event data types in verboseservice.
    Adapt for the named structs instead of map[string]...  Simplify some
    message string generation logic along the way.
    
    NOTE: The same treatment does not apply to the lib/rc package.  That
    one uses data unmarshalled from JSON, thus always works with
    map[string]interface{}.

--- lib/syncthing/verboseservice.go
+++ lib/syncthing/verboseservice.go
@@ -11,8 +11,12 @@ import (
 	"fmt"
 	"regexp"
 
+	"github.com/syncthing/syncthing/lib/api"
+	"github.com/syncthing/syncthing/lib/connections"
+	"github.com/syncthing/syncthing/lib/discover"
 	"github.com/syncthing/syncthing/lib/events"
 	"github.com/syncthing/syncthing/lib/model"
+	"github.com/syncthing/syncthing/lib/scanner"
 )
 
 // The verbose logging service subscribes to events and prints these in
@@ -57,127 +61,111 @@ func (s *verboseService) formatEvent(ev events.Event) string {
 		return ""
 
 	case events.Starting:
-		return fmt.Sprintf("Starting up (%s)", ev.Data.(map[string]string)["home"])
+		data := ev.Data.(StartingEventData)
+		return fmt.Sprintf("Starting up (%s)", data.Home)
 
 	case events.StartupComplete:
 		return "Startup complete"
 
 	case events.DeviceDiscovered:
-		data := ev.Data.(map[string]interface{})
-		return fmt.Sprintf("Discovered device %v at %v", data["device"], data["addrs"])
+		data := ev.Data.(discover.DeviceDiscoveredEventData)
+		return fmt.Sprintf("Discovered device %v at %v", data.Device, data.Addresses)
 
 	case events.DeviceConnected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Connected to device %v at %v (type %s)", data["id"], data["addr"], data["type"])
+		data := ev.Data.(model.DeviceConnectedEventData)
+		return fmt.Sprintf("Connected to device %v at %v (type %s)", data.ID, data.Address, data.Type)
 
 	case events.DeviceDisconnected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Disconnected from device %v", data["id"])
+		data := ev.Data.(model.DeviceDisconnectedEventData)
+		return fmt.Sprintf("Disconnected from device %v", data.ID)
 
 	case events.StateChanged:
-		data := ev.Data.(map[string]interface{})
-		return fmt.Sprintf("Folder %q is now %v", data["folder"], data["to"])
+		data := ev.Data.(model.StateChangedEventData)
+		return fmt.Sprintf("Folder %q is now %v", data.Folder, data.To)
 
 	case events.LocalChangeDetected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Local change detected in folder %q: %s %s %s", data["folder"], data["action"], data["type"], data["path"])
+		data := ev.Data.(model.DiskChangeDetectedEventData)
+		return fmt.Sprintf("Local change detected in folder %q: %s %s %s", data.Folder, data.Action, data.Type, data.Path)
 
 	case events.RemoteChangeDetected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Remote change detected in folder %q: %s %s %s", data["folder"], data["action"], data["type"], data["path"])
+		data := ev.Data.(model.DiskChangeDetectedEventData)
+		return fmt.Sprintf("Remote change detected in folder %q: %s %s %s", data.Folder, data.Action, data.Type, data.Path)
 
 	case events.RemoteIndexUpdated:
-		data := ev.Data.(map[string]interface{})
-		return fmt.Sprintf("Device %v sent an index update for %q with %d items", data["device"], data["folder"], data["items"])
+		data := ev.Data.(model.RemoteIndexUpdatedEventData)
+		return fmt.Sprintf("Device %v sent an index update for %q with %d items", data.Device, data.Folder, data.Items)
 
 	case events.DeviceRejected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Rejected connection from device %v at %v", data["device"], data["address"])
+		data := ev.Data.(model.DeviceRejectedEventData)
+		return fmt.Sprintf("Rejected connection from device %v at %v", data.Device, data.Address)
 
 	case events.FolderRejected:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Rejected unshared folder %q from device %v", data["folder"], data["device"])
+		data := ev.Data.(model.FolderRejectedEventData)
+		return fmt.Sprintf("Rejected unshared folder %q from device %v", data.Folder, data.Device)
 
 	case events.ItemStarted:
-		data := ev.Data.(map[string]string)
-		return fmt.Sprintf("Started syncing %q / %q (%v %v)", data["folder"], data["item"], data["action"], data["type"])
+		data := ev.Data.(model.ItemStartedEventData)
+		return fmt.Sprintf("Started syncing %q / %q (%v %v)", data.Folder, data.Item, data.Action, data.Type)
 
 	case events.ItemFinished:
-		data := ev.Data.(map[string]interface{})
-		if err, ok := data["error"].(*string); ok && err != nil {
-			// If the err interface{} is not nil, it is a string pointer.
-			// Dereference it to get the actual error or Sprintf will print
-			// the pointer value....
-			return fmt.Sprintf("Finished syncing %q / %q (%v %v): %v", data["folder"], data["item"], data["action"], data["type"], *err)
+		data := ev.Data.(model.ItemFinishedEventData)
+		err := "Success"
+		if data.Error != nil {
+			err = *data.Error
 		}
-		return fmt.Sprintf("Finished syncing %q / %q (%v %v): Success", data["folder"], data["item"], data["action"], data["type"])
+		return fmt.Sprintf("Finished syncing %q / %q (%v %v): %v", data.Folder, data.Item, data.Action, data.Type, err)
 
 	case events.ConfigSaved:
 		return "Configuration was saved"
 
 	case events.FolderCompletion:
-		data := ev.Data.(map[string]interface{})
-		return fmt.Sprintf("Completion for folder %q on device %v is %v%% (state: %s)", data["folder"], data["device"], data["completion"], data["remoteState"])
+		data := ev.Data.(model.FolderCompletionEventData)
+		return fmt.Sprintf("Completion for folder %q on device %v is %v%% (state: %s)", data.Folder, data.Device, data.CompletionPct, data.RemoteState)
 
 	case events.FolderSummary:
 		data := ev.Data.(model.FolderSummaryEventData)
 		return folderSummaryRemoveDeprecatedRe.ReplaceAllString(fmt.Sprintf("Summary for folder %q is %+v", data.Folder, data.Summary), "")
 
 	case events.FolderScanProgress:
-		data := ev.Data.(map[string]interface{})
-		folder := data["folder"].(string)
-		current := data["current"].(int64)
-		total := data["total"].(int64)
-		rate := data["rate"].(float64) / 1024 / 1024
+		data := ev.Data.(scanner.FolderScanProgressEventData)
+		rate := data.Rate / 1024 / 1024
 		var pct int64
-		if total > 0 {
-			pct = 100 * current / total
+		if data.Total > 0 {
+			pct = 100 * data.Current / data.Total
 		}
-		return fmt.Sprintf("Scanning folder %q, %d%% done (%.01f MiB/s)", folder, pct, rate)
+		return fmt.Sprintf("Scanning folder %q, %d%% done (%.01f MiB/s)", data.Folder, pct, rate)
 
 	case events.DevicePaused:
-		data := ev.Data.(map[string]string)
-		device := data["device"]
-		return fmt.Sprintf("Device %v was paused", device)
+		data := ev.Data.(model.DevicePausedEventData)
+		return fmt.Sprintf("Device %v was paused", data.Device)
 
 	case events.DeviceResumed:
-		data := ev.Data.(map[string]string)
-		device := data["device"]
-		return fmt.Sprintf("Device %v was resumed", device)
+		data := ev.Data.(model.DeviceResumedEventData)
+		return fmt.Sprintf("Device %v was resumed", data.Device)
 
 	case events.ClusterConfigReceived:
 		data := ev.Data.(model.ClusterConfigReceivedEventData)
 		return fmt.Sprintf("Received ClusterConfig from device %v", data.Device)
 
 	case events.FolderPaused:
-		data := ev.Data.(map[string]string)
-		id := data["id"]
-		label := data["label"]
-		return fmt.Sprintf("Folder %v (%v) was paused", id, label)
+		data := ev.Data.(model.FolderPausedEventData)
+		return fmt.Sprintf("Folder %v (%v) was paused", data.ID, data.Label)
 
 	case events.FolderResumed:
-		data := ev.Data.(map[string]string)
-		id := data["id"]
-		label := data["label"]
-		return fmt.Sprintf("Folder %v (%v) was resumed", id, label)
+		data := ev.Data.(model.FolderResumedEventData)
+		return fmt.Sprintf("Folder %v (%v) was resumed", data.ID, data.Label)
 
 	case events.ListenAddressesChanged:
-		data := ev.Data.(map[string]interface{})
-		address := data["address"]
-		lan := data["lan"]
-		wan := data["wan"]
-		return fmt.Sprintf("Listen address %s resolution has changed: lan addresses: %s wan addresses: %s", address, lan, wan)
+		data := ev.Data.(connections.ListenAddressesChangedEventData)
+		return fmt.Sprintf("Listen address %s resolution has changed: lan addresses: %s wan addresses: %s", data.Address, data.LAN, data.WAN)
 
 	case events.LoginAttempt:
-		data := ev.Data.(map[string]interface{})
-		username := data["username"].(string)
-		var success string
-		if data["success"].(bool) {
+		data := ev.Data.(api.LoginAttemptEventData)
+		success := "failed"
+		if data.Success {
 			success = "successful"
-		} else {
-			success = "failed"
 		}
-		return fmt.Sprintf("Login %s for username %s.", success, username)
+		return fmt.Sprintf("Login %s for username %s.", success, data.Username)
 	}
 
 	return fmt.Sprintf("%s %#v", ev.Type, ev)
