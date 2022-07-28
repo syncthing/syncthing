ca67eaeee84e0a17dc8a55c210c36005254df85c
Jakob Borg <jakob@kastelo.net>
2022-07-28 10:33:52 +0200

    wip

--- lib/model/folder.go
+++ lib/model/folder.go
@@ -646,6 +646,7 @@ func (f *folder) scanSubdirsChangedAndNew(subDirs []string, batch *scanBatch) (i
 		LocalFlags:            f.localFlags,
 		ModTimeWindow:         f.modTimeWindow,
 		EventLogger:           f.evLogger,
+		XattrFilter:           f.XattrFilter,
 	}
 	var fchan chan scanner.ScanResult
 	if f.Type == config.FolderTypeReceiveEncrypted {
