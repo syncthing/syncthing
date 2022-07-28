55034531f0102bf688642d0728dfc4ffeb5dc4a2
Jakob Borg <jakob@kastelo.net>
2022-07-28 16:04:15 +0200

    wip

--- lib/config/config_test.go
+++ lib/config/config_test.go
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+// Here too is an inconsequential change.
+
 package config
 
 import (
