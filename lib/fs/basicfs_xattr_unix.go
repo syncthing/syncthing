3b6b37819c85a76356aac97bb03a1acf6c2b3f53
Jakob Borg <jakob@kastelo.net>
2022-08-08 11:32:39 +0200

    wip

--- lib/fs/basicfs_xattr_unix.go
+++ lib/fs/basicfs_xattr_unix.go
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build !windows
-// +build !windows
+//go:build !windows && !dragonfly
+// +build !windows,!dragonfly
 
 package fs
 
---
+++ lib/fs/basicfs_xattr_unsupported.go
@@ -0,0 +1,22 @@
+// Copyright (C) 2022 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
+//go:build dragonfly
+// +build dragonfly
+
+package fs
+
+import (
+	"github.com/syncthing/syncthing/lib/protocol"
+)
+
+func (f *BasicFilesystem) GetXattr(path string, xattrFilter StringFilter) ([]protocol.Xattr, error) {
+	return nil, nil
+}
+
+func (f *BasicFilesystem) SetXattr(path string, xattrs []protocol.Xattr, xattrFilter StringFilter) error {
+	return nil
+}
