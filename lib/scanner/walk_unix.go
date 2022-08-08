ccc60504294987a761b845a016c22b4c287e5495
Jakob Borg <jakob@kastelo.net>
2022-08-08 11:17:06 +0200

    wip

--- lib/scanner/walk_unix.go
+++ lib/scanner/walk_unix.go
@@ -1,3 +1,9 @@
+// Copyright (C) 2022 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 //go:build !windows
 // +build !windows
 
