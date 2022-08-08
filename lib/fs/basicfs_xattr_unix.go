38e36a66fff085368f20ec1f73cacaaf2486fc72
Jakob Borg <jakob@kastelo.net>
2022-08-08 11:49:47 +0200

    wip

--- lib/fs/basicfs_xattr_unix.go
+++ lib/fs/basicfs_xattr_unix.go
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build !windows && !dragonfly && !illumos && !solaris
-// +build !windows,!dragonfly,!illumos,!solaris
+//go:build !windows && !dragonfly && !illumos && !solaris && !openbsd
+// +build !windows,!dragonfly,!illumos,!solaris,!openbsd
 
 package fs
 
--- lib/fs/basicfs_xattr_unsupported.go
+++ lib/fs/basicfs_xattr_unsupported.go
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-//go:build dragonfly || illumos || solaris
-// +build dragonfly illumos solaris
+//go:build dragonfly || illumos || solaris || openbsd
+// +build dragonfly illumos solaris openbsd
 
 package fs
 
