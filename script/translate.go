06767fbdb53e0d019f8a3306a45e7cbd2401e604
Tomasz Wilczyński <twilczynski@naver.com>
2022-09-12 21:49:37 +0200

    wip
    Signed-off-by: Tomasz Wilczyński <twilczynski@naver.com>

--- script/translate.go
+++ script/translate.go
@@ -27,7 +27,7 @@ var attrReCond = regexp.MustCompile(`\{\{.+\s+\?\s+'([^']+)'\s+:\s+'([^']+)'\s+\
 
 // Find both $translate.instant("…") and $translate.instant("…",…) in JS.
 // Consider single quote variants too.
-var jsRe = regexp.MustCompile(`\$translate.instant\((?:"|')(.+?)(?:"|')(?:, \{.+\})*?\)`)
+var jsRe = regexp.MustCompile(`\$translate.instant\((?:"|')(.+?)(?:"|')(?:, *?\{.+\})*?\)`)
 
 // exceptions to the untranslated text warning
 var noStringRe = regexp.MustCompile(
