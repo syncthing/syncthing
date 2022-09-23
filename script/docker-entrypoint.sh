a80e35c140fd3046cb19435cd6041f4b7792d4cf
Jakob Borg <jakob@kastelo.net>
2022-09-23 11:38:57 +0200

    spaces

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -7,10 +7,10 @@ if [ "$PCAP" == "" ] ; then
   # If Syncthing should have no extra capabilities, make sure to remove them
   # from the binary. This will fail with an error if there are no
   # capabilities to remove, hence the || true etc.
-	setcap -r "$binary" 2>/dev/null || true
+  setcap -r "$binary" 2>/dev/null || true
 else
   # Set capabilities on the Syncthing binary before launching it.
-	setcap "$PCAP" "$binary"
+  setcap "$PCAP" "$binary"
 fi
 
 if [ "$(id -u)" = '0' ]; then
