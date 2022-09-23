04b38595209d34df824db110c368e74112016d83
Jakob Borg <jakob@kastelo.net>
2022-09-23 11:40:08 +0200

    onlyasroot

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 
 set -eu
 
-binary="$1"
-if [ "$PCAP" == "" ] ; then
-  # If Syncthing should have no extra capabilities, make sure to remove them
-  # from the binary. This will fail with an error if there are no
-  # capabilities to remove, hence the || true etc.
-  setcap -r "$binary" 2>/dev/null || true
-else
-  # Set capabilities on the Syncthing binary before launching it.
-  setcap "$PCAP" "$binary"
-fi
-
 if [ "$(id -u)" = '0' ]; then
+  binary="$1"
+  if [ "$PCAP" == "" ] ; then
+    # If Syncthing should have no extra capabilities, make sure to remove them
+    # from the binary. This will fail with an error if there are no
+    # capabilities to remove, hence the || true etc.
+    setcap -r "$binary" 2>/dev/null || true
+  else
+    # Set capabilities on the Syncthing binary before launching it.
+    setcap "$PCAP" "$binary"
+  fi
+
   chown "${PUID}:${PGID}" "${HOME}" \
     && exec su-exec "${PUID}:${PGID}" \
        env HOME="$HOME" "$@"
