b45a171523e3135b6e0ca939ae3ea55cf2c2702f
Jakob Borg <jakob@kastelo.net>
2023-06-27 11:02:26 +0200

    cmd/syncthing: Add environment variables for --home, --conf, and --data
    This allows environment overrides for our directories. This is
    advantageous because, apart from the obvious, it means we can set it in
    the Docker file and not add command line options there. Having the
    command line option as we did meant that it was impossible to use the
    Docker image for other commands than `serve` (because that is implied
    when we see other options on the command line).

--- cmd/syncthing/cmdutil/options_common.go
+++ cmd/syncthing/cmdutil/options_common.go
@@ -9,8 +9,8 @@ package cmdutil
 // CommonOptions are reused among several subcommands
 type CommonOptions struct {
 	buildCommonOptions
-	ConfDir         string `name:"config" placeholder:"PATH" help:"Set configuration directory (config and keys)"`
-	HomeDir         string `name:"home" placeholder:"PATH" help:"Set configuration and data directory"`
+	ConfDir         string `name:"config" placeholder:"PATH" env:"STCONFDIR" help:"Set configuration directory (config and keys)"`
+	HomeDir         string `name:"home" placeholder:"PATH" env:"STHOMEDIR" help:"Set configuration and data directory"`
 	NoDefaultFolder bool   `env:"STNODEFAULTFOLDER" help:"Don't create the \"default\" folder on first startup"`
 	SkipPortProbing bool   `help:"Don't try to find free ports for GUI and listen addresses on first startup"`
 }
