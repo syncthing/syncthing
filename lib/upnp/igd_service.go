5a16ea451348ee1bbe90acc2495a1678f7090746
Maximilian R <maxi.rostock@outlook.de>
2023-07-30 21:55:13 +0200

    do not guess protocol based on LocalIP

--- lib/upnp/igd_service.go
+++ lib/upnp/igd_service.go
@@ -154,13 +154,9 @@ func (s *IGDService) GetLocalIPAddress() net.IP {
 	return s.LocalIP
 }
 
-// IsIPv6 checks whether the local IP, which is detected by trying to connect to the gateway is an IPv6 addr.
-// This is equivalent to the gateway being IPv6. We use this to decide whether port mapping or pinholing should
-// be tried, because the former usually makes more sense on IPv4 setups while the latter is only allowed on IPv6
-// The surrounding infrastructure creates a separate IGDService vor IPv4 and IPv6 if both are available, so we only
-// need to concern ourselves with one.
+// IsIPv6 checks whether this is a WANIPv6FirewallControl device, in which case pinholing instead of port mapping should be done
 func (s *IGDService) IsIPv6() bool {
-	return s.LocalIP.To4() == nil
+	return s.URN == "urn:schemas-upnp-org:service:WANIPv6FirewallControl:1"
 }
 
 // ID returns a unique ID for the servic
