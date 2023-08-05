306028827180ea8916b898f8e1670c74d3212d47
Maximilian R <maxi.rostock@outlook.de>
2023-08-05 17:47:21 +0200

    actually use the ip

--- lib/upnp/igd_service.go
+++ lib/upnp/igd_service.go
@@ -117,7 +117,7 @@ func (s *IGDService) tryAddPinholeForIP6(ctx context.Context, protocol nat.Proto
 	<LeaseTime>%d</LeaseTime>
 	</u:AddPinhole>`
 
-	body := fmt.Sprintf(template, s.URN, protoNumber, port, s.LocalIP, duration/time.Second)
+	body := fmt.Sprintf(template, s.URN, protoNumber, port, ip, duration/time.Second)
 
 	_, err := soapRequest(ctx, s.URL, s.URN, "AddPinhole", body)
 
