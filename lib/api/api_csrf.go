syncthing/syncthing

commit 7c038fd8d68f943334fa944d199d31e16d195ca6

Extract function api.isNoAuthPath
(This is known to break TestCSRFRequired).

lib/api/api_csrf.go
@@ -74,7 +74,7 @@ func (m *csrfManager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if strings.HasPrefix(r.URL.Path, "/rest/noauth") || r.URL.Path == "/rest/svc/lang" { // FIXME: this duplicates some logic in basicAuthAndSessionMiddleware in api_auth.go
+	if isNoAuthPath(r.URL.Path) {
 		// REST calls that don't require authentication also do not
 		// need a CSRF token.
 		m.next.ServeHTTP(w, r)

lib/api/api_auth.go
@@ -92,6 +92,10 @@ func noAuthPrefixes() []string {
 	}
 }
 
+func isNoAuthPath(path string) bool {
+	return equalsAny(path, noAuthPaths()) || hasAnyPrefix(path, noAuthPrefixes())
+}
+
 func basicAuthAndSessionMiddleware(cookieName string, guiCfg config.GUIConfiguration, ldapCfg config.LDAPConfiguration, next http.Handler, evLogger events.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if hasValidAPIKeyHeader(r, guiCfg) {
@@ -100,7 +104,7 @@ func basicAuthAndSessionMiddleware(cookieName string, guiCfg config.GUIConfigura
 		}
 
 		// Exception for static assets and REST calls that don't require authentication.
-		if equalsAny(r.URL.Path, noAuthPaths()) || hasAnyPrefix(r.URL.Path, noAuthPrefixes()) {
+		if isNoAuthPath(r.URL.Path) {
 			next.ServeHTTP(w, r)
 			return
 		}

commit 797cd0e534417dbb56dd8c71b62cfd82a34b8d5f

Check noAuthPath after CSRF prefix
This fixes the TestCSRFRequired failure introduced in commit
7c038fd8d68f943334fa944d199d31e16d195ca6.

lib/api/api_csrf.go
@@ -74,13 +74,6 @@ func (m *csrfManager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if isNoAuthPath(r.URL.Path) {
-		// REST calls that don't require authentication also do not
-		// need a CSRF token.
-		m.next.ServeHTTP(w, r)
-		return
-	}
-
 	// Allow requests for anything not under the protected path prefix,
 	// and set a CSRF cookie if there isn't already a valid one.
 	if !strings.HasPrefix(r.URL.Path, m.prefix) {
@@ -93,6 +86,13 @@ func (m *csrfManager) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 			http.SetCookie(w, cookie)
 		}
+		m.next.ServeHTTP(w, r)
+		return
+	}
+
+	if isNoAuthPath(r.URL.Path) {
+		// REST calls that don't require authentication also do not
+		// need a CSRF token.
 		m.next.ServeHTTP(w, r)
 		return
 	}