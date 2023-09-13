4dc59e3be92e56ebf988d9fc4902c7625b0a9a41
Maximilian <maxi.rostock@outlook.de>
2023-09-13 18:30:55 +0200

    Apply suggestion
    Co-authored-by: André Colomb <github.com@andre.colomb.de>

--- lib/nat/structs.go
+++ lib/nat/structs.go
@@ -86,7 +86,7 @@ func (m *Mapping) OnChanged(subscribed MappingChangeSubscriber) {
 }
 
 func (m *Mapping) String() string {
-	return fmt.Sprintf("%s/%s", m.protocol, m.address)
+	return fmt.Sprintf("%s/%s", m.address, m.protocol)
 }
 
 func (m *Mapping) GoString() string {
