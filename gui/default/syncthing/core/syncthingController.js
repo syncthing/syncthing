e46dbe151bd107742a17839aee2baa0df95aa0a1
Donovan Käfer @d-volution <it.don@pm.me>
2023-09-25 16:44:09 +0200

    gui: Scroll to bottom by clicking message in log viewer

--- gui/default/syncthing/core/syncthingController.js
+++ gui/default/syncthing/core/syncthingController.js
@@ -1429,6 +1429,11 @@ angular.module('syncthing.core')
                 // Browser events do not cause redraw, trigger manually.
                 $scope.$apply();
             },
+            scrollToBottom: function () {
+                var textArea = $('#logViewerText');
+                var scrollHeight = textArea.prop('scrollHeight');
+                textArea.prop('scrollTop', scrollHeight);
+            },
             timer: null,
             entries: [],
             paused: false,
