6e0b60b73fd53ea46417a24125be194d8a6b2af3
Emil Lundberg <emil@emlun.se>
2023-10-06 12:26:16 +0200

    fixup! Address review comments

--- lib/api/api_auth.go
+++ lib/api/api_auth.go
@@ -11,7 +11,6 @@ import (
 	"fmt"
 	"net"
 	"net/http"
-	"slices"
 	"strings"
 	"time"
 
@@ -20,6 +19,7 @@ import (
 	"github.com/syncthing/syncthing/lib/events"
 	"github.com/syncthing/syncthing/lib/rand"
 	"github.com/syncthing/syncthing/lib/sync"
+	"golang.org/x/exp/slices"
 )
 
 var (
