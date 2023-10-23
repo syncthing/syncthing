7febf8880730ec037f03bb3832d21ce347573a74
Jakob Borg <jakob@kastelo.net>
2023-10-23 09:52:50 +0200

    wip

--- lib/locations/locations_test.go
+++ lib/locations/locations_test.go
@@ -4,6 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
+//go:build !windows
+
 package locations
 
 import (
