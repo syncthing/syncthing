43b43ef38985f3dab69d87cbc03f7c707bbde5a9
Vishal A. Patel <vishal.patel2@hp.com>
2023-11-07 17:00:17 +0000

    Always call net.ParseIP(), regardless if comma is found

--- cmd/stdiscosrv/apisrv.go
+++ cmd/stdiscosrv/apisrv.go
@@ -137,11 +137,10 @@ func (s *apiSrv) handler(w http.ResponseWriter, req *http.Request) {
 
 	if s.useHTTP {
 		// X-Forwarded-For can have multiple client IPs; split using the comma separator
-		forwardIP, _, found := strings.Cut(req.Header.Get("X-Forwarded-For"), ",")
-		if found {
-			// net.ParseIP will return nil if leading/trailing whitespace exists; use strings.TrimSpace()
-			remoteAddr.IP = net.ParseIP(strings.TrimSpace(forwardIP))
-		}
+		forwardIP, _, _ := strings.Cut(req.Header.Get("X-Forwarded-For"), ",")
+
+		// net.ParseIP will return nil if leading/trailing whitespace exists; use strings.TrimSpace()
+		remoteAddr.IP = net.ParseIP(strings.TrimSpace(forwardIP))
 
 		if parsedPort, err := strconv.ParseInt(req.Header.Get("X-Client-Port"), 10, 0); err == nil {
 			remoteAddr.Port = int(parsedPort)
