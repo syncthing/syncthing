b8cda97c981fc814949b170ce9469c4fb16e9689
Jakob Borg <jakob@kastelo.net>
2023-11-13 11:33:34 +0100

    lib/api: Add cache busting for basic auth (ref #9208)
    This adds our short device ID to the basic auth realm. This has at least
    two consequences:
    
    - It is different from what's presented by another device on the same
      address (e.g., if I use SSH forwards to different dives on the same
      local address), preventing credentials for one from being sent to
      another.
    
    - It is different from what we did previously, meaning we avoid cached
      credentials from old versions interfering with the new login flow.
    
    I don't *think* there should be things that depend on our precise realm
    string, so this shouldn't break any existing setups...

--- lib/api/api.go
+++ lib/api/api.go
@@ -372,8 +372,8 @@ func (s *service) Serve(ctx context.Context) error {
 
 	// Wrap everything in basic auth, if user/password is set.
 	if guiCfg.IsAuthEnabled() {
-		sessionCookieName := "sessionid-" + s.id.String()[:5]
-		handler = basicAuthAndSessionMiddleware(sessionCookieName, guiCfg, s.cfg.LDAP(), handler, s.evLogger)
+		sessionCookieName := "sessionid-" + s.id.Short().String()
+		handler = basicAuthAndSessionMiddleware(sessionCookieName, s.id.Short().String(), guiCfg, s.cfg.LDAP(), handler, s.evLogger)
 		handlePasswordAuth := passwordAuthHandler(sessionCookieName, guiCfg, s.cfg.LDAP(), s.evLogger)
 		restMux.Handler(http.MethodPost, "/rest/noauth/auth/password", handlePasswordAuth)
 
