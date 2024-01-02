482c20dd20faca774bfc94b0dc43d436cdc2f4aa
Eric P <eric@kastelo.net>
2024-01-02 18:08:53 +0100

    Add copyright notes

--- cmd/ursrv/aggregate/formatting.go
+++ cmd/ursrv/aggregate/formatting.go
@@ -1,3 +1,9 @@
+// Copyright (C) 2024 The Syncthing Authors.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/.
+
 package aggregate
 
 import (
