40a024a57d35c9a9e61f6ef0b35ebf43131c4bd6
Jakob Borg <jakob@kastelo.net>
2024-02-09 16:26:01 +0100

    wip

--- lib/connections/quic_unsupported.go
+++ lib/connections/quic_unsupported.go
@@ -4,8 +4,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-//go:build noquic || !go1.15
-// +build noquic !go1.15
+//go:build noquic
+// +build noquic
 
 package connections
 
