5d5186c86137dd228d86e1005fe30e1ab9fc7386
Ross Smith II <ross@smithii.com>
2024-05-20 21:32:45 -0700

    gui: Use localised time in duration
    https://github.com/syncthing/syncthing/pull/8291 inpired me to
    develop this. I tested it with all the languages Syncthing
    currently supports, and they all work.
    
    The only issue is that when you change the language in the gui,
    you have to either refresh the page, or wait a few seconds, for
    the page to refresh by itself, before the duration is translated
    into the new language.

--- gui/default/syncthing/core/durationFilter.js
+++ gui/default/syncthing/core/durationFilter.js
@@ -7,11 +7,57 @@
  * {{1|duration:"h"}}         --> <1h
 **/
 angular.module('syncthing.core')
-    .filter('duration', function () {
+    .filter('duration', function ($translate) {
         'use strict';
 
         var SECONDS_IN = { "d": 86400, "h": 3600, "m": 60, "s": 1 };
         return function (input, precision) {
+            var milliseconds = parseInt(input, 10) * 1000;
+            var units = ["d", "h", "m", "s"];
+            switch (precision) {
+                case "m":
+                    units = ["d", "h", "m"];
+                    break
+                case "h":
+                    units = ["d", "h"];
+                    break
+                case "d":
+                    units = ["d"];
+                    break
+                case "s":
+                    break
+                default:
+                    return "[Error: incorrect usage, precision must be one of d, h, m or s]";
+            }
+
+            let language_cc = $translate.use();
+            if (language_cc != null) {
+                language_cc = language_cc.replace("-", "_");
+                var fallbacks = [];
+                var language = language_cc.substr(0, 2);
+                switch (language) {
+                case "zh":
+                    // Use zh_TW for zh_HK
+                    fallbacks.push("zh_TW");
+                    break
+                }
+                if (language != language_cc) {
+                    fallbacks.push(language);
+                }
+                // Fallback to english, if the language isn't found
+                fallbacks.push("en");
+                try {
+                    return humanizeDuration(milliseconds, {
+                        language: language_cc,
+                        maxDecimalPoints: 0,
+                        units: units,
+                        fallbacks: fallbacks
+                    });
+                } catch(err) {
+                    console.log(err.message + ": language_cc=" + language_cc)
+                    // if we crash, fallthrough to english
+                }
+            }
             var result = "";
             if (!precision) {
                 precision = "s";
