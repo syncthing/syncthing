218ac23b7d530aae6b0f26bac5cdc172fe7040da
André Colomb <src@andre.colomb.de>
2024-09-01 17:29:34 +0200

    Rename accumulators

--- lib/api/api.go
+++ lib/api/api.go
@@ -1483,11 +1483,11 @@ func (*service) getDeviceID(w http.ResponseWriter, r *http.Request) {
 
 func (*service) getLang(w http.ResponseWriter, r *http.Request) {
 	lang := r.Header.Get("Accept-Language")
-	var langs = make(map[string]float64)
+	var weights = make(map[string]float64)
 	for _, l := range strings.Split(lang, ",") {
 		parts := strings.SplitN(l, ";", 2)
 		code := strings.ToLower(strings.TrimSpace(parts[0]))
-		langs[code] = 1.0
+		weights[code] = 1.0
 		if len(parts) < 2 {
 			continue
 		}
@@ -1497,20 +1497,20 @@ func (*service) getLang(w http.ResponseWriter, r *http.Request) {
 		}
 		if q, err := strconv.ParseFloat(weight[2:], 32); err != nil {
 			// Completely dismiss entries with invalid weight
-			delete(langs, code)
+			delete(weights, code)
 		} else {
-			langs[code] = q
+			weights[code] = q
 		}
 	}
-	var orderedLangs = make([]string, 0, len(langs))
-	for code := range langs {
-		orderedLangs = append(orderedLangs, code)
+	var langs = make([]string, 0, len(weights))
+	for code := range weights {
+		langs = append(langs, code)
 	}
 	// Reorder by descending q value
-	sort.SliceStable(orderedLangs, func(i, j int) bool {
-		return langs[orderedLangs[i]] > langs[orderedLangs[j]]
+	sort.SliceStable(langs, func(i, j int) bool {
+		return weights[langs[i]] > weights[langs[j]]
 	})
-	sendJSON(w, orderedLangs)
+	sendJSON(w, langs)
 }
 
 func (s *service) postSystemUpgrade(w http.ResponseWriter, _ *http.Request) {
