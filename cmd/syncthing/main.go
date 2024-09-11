e0715cff31baf42344f399191e521c4549f6753d
Ulrich Hornung <hornunguli@gmx.de>
2024-09-11 21:13:55 +0200

    revert update disabled hard-coded - use config instead

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -457,8 +457,6 @@ func (e *errNoUpgrade) Error() string {
 }
 
 func checkUpgrade() (upgrade.Release, error) {
-	return upgrade.Release{}, &errNoUpgrade{"ofuse", "ofuse"}
-
 	cfg, err := loadOrDefaultConfig()
 	if err != nil {
 		return upgrade.Release{}, err
