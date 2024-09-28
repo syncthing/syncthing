a19cd8dc8ce1747b2ecc9064ec7af1b3447a918d
Jakob Borg <jakob@kastelo.net>
2024-09-28 16:44:05 +0200

    wip

--- lib/ur/usage_report.go
+++ lib/ur/usage_report.go
@@ -26,7 +26,6 @@ import (
 	"github.com/syncthing/syncthing/lib/db"
 	"github.com/syncthing/syncthing/lib/dialer"
 	"github.com/syncthing/syncthing/lib/protocol"
-	srand "github.com/syncthing/syncthing/lib/rand"
 	"github.com/syncthing/syncthing/lib/scanner"
 	"github.com/syncthing/syncthing/lib/upgrade"
 	"github.com/syncthing/syncthing/lib/ur/contract"
@@ -384,20 +383,6 @@ func (s *Service) Serve(ctx context.Context) error {
 			t.Reset(0)
 		case <-t.C:
 			if s.cfg.Options().URAccepted >= 2 {
-				if s.cfg.Options().URUniqueID == "" {
-					// Set a unique ID if this is the first time we're
-					// sending a report
-					w, err := s.cfg.Modify(func(cfg *config.Configuration) {
-						cfg.Options.URUniqueID = srand.String(8)
-					})
-					if err != nil {
-						// Hope for better luck next time, no need to try to
-						// send a report without a unique ID
-						continue
-					}
-					w.Wait()
-				}
-
 				err := s.sendUsageReport(ctx)
 				if err != nil {
 					l.Infoln("Usage report:", err)
