479c0d3f169c3f949786bc487f05364999e10c6a
Alex Ionescu <github@ionescu.sh>
2024-12-08 21:20:31 +0100

    fix(gui): reflect folder password visibility by changing button icon (#9857)
    Make the "toggle password visibility" button on encrypted
    folders change its icon from `fa-eye` to `fa-eye-slash` while the
    password is visible.

--- gui/default/syncthing/core/syncthingController.js
+++ gui/default/syncthing/core/syncthingController.js
@@ -3703,7 +3703,6 @@ angular.module('syncthing.core')
                 untrusted: '=',
             },
             link: function (scope, elem, attrs) {
-                var plain = false;
                 scope.togglePasswordVisibility = function() {
                     scope.plain = !scope.plain;
                 };
