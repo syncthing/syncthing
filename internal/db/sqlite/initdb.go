8eca619f03019c6e1d0a9d6301cfe66214b51e94
Jakob Borg <jakob@kastelo.net>
2025-02-23 17:14:35 +0100

    wip

--- internal/db/sqlite/initdb.go
+++ internal/db/sqlite/initdb.go
@@ -1,38 +1,16 @@
 package sqlite
 
 import (
-	"bytes"
 	"embed"
 	"fmt"
 	"io/fs"
 	"strings"
-	"text/template"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/syncthing/syncthing/lib/protocol"
 )
 
 //go:embed init/*.sql
 var initScripts embed.FS
-
-var tplParams = map[string]any{
-	"FileInfoTypes": []int64{
-		int64(protocol.FileInfoTypeFile),
-		int64(protocol.FileInfoTypeDirectory),
-		int64(protocol.FileInfoTypeSymlink),
-	},
-	"LocalFlagBits": []int64{
-		0, // no flags set
-		protocol.FlagLocalUnsupported,
-		protocol.FlagLocalIgnored,
-		protocol.FlagLocalMustRescan,
-		protocol.FlagLocalReceiveOnly,
-		protocol.FlagLocalGlobal,
-		protocol.FlagLocalNeeded,
-		protocol.FlagLocalDeleted,
-		protocol.FlagLocalGlobal | protocol.FlagLocalNeeded,
-	},
-}
 
 func initDB(db *sqlx.DB) error {
 	initFs, err := fs.Sub(initScripts, "init")
@@ -48,12 +26,7 @@ func initDB(db *sqlx.DB) error {
 		if err != nil {
 			return wrap("init", err)
 		}
-		tpl := template.Must(template.New(scr.Name()).Parse(string(bs)))
-		buf := new(bytes.Buffer)
-		if err := tpl.Execute(buf, tplParams); err != nil {
-			return wrap("init", err)
-		}
-		for _, stmt := range strings.Split(buf.String(), "\n;") {
+		for _, stmt := range strings.Split(string(bs), "\n;") {
 			if _, err := db.Exec(stmt); err != nil {
 				return fmt.Errorf("init statement: %s: %w", stmt, err)
 			}
