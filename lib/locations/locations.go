98254f939578a48dbe25b5bfc0bd14f8448ba64f
Ulrich Hornung <hornunguli@gmx.de>
2025-03-16 16:50:19 +0100

    by default, use seperate config directories to syncthing (#7)
    * by default, use seperate config directories to syncthing
    
    * fix config tests
    
    * disabled debian package build

--- lib/locations/locations.go
+++ lib/locations/locations.go
@@ -48,8 +48,8 @@ const (
 
 	LevelDBDir          = "index-v0.14.0.db"
 	configFileName      = "config.xml"
-	defaultStateDir     = ".local/state/syncthing"
-	oldDefaultConfigDir = ".config/syncthing"
+	defaultStateDir     = ".local/state/thingium"
+	oldDefaultConfigDir = ".config/thingium"
 )
 
 // Platform dependent directories
@@ -200,13 +200,13 @@ func defaultDataDir(userHome, configDir string) string {
 
 func windowsConfigDataDir() string {
 	if p := os.Getenv("LocalAppData"); p != "" {
-		return filepath.Join(p, "Syncthing")
+		return filepath.Join(p, "Thingium")
 	}
-	return filepath.Join(os.Getenv("AppData"), "Syncthing")
+	return filepath.Join(os.Getenv("AppData"), "Thingium")
 }
 
 func darwinConfigDataDir(userHome string) string {
-	return filepath.Join(userHome, "Library/Application Support/Syncthing")
+	return filepath.Join(userHome, "Library/Application Support/Thingium")
 }
 
 func unixConfigDir(userHome, xdgConfigHome, xdgStateHome string, fileExists func(string) bool) string {
@@ -215,7 +215,7 @@ func unixConfigDir(userHome, xdgConfigHome, xdgStateHome string, fileExists func
 	// ignored, but that's not what we did previously, so we retain the
 	// old behavior.
 	if xdgConfigHome != "" {
-		candidate := filepath.Join(xdgConfigHome, "syncthing")
+		candidate := filepath.Join(xdgConfigHome, "thingium")
 		if fileExists(filepath.Join(candidate, configFileName)) {
 			return candidate
 		}
@@ -229,7 +229,7 @@ func unixConfigDir(userHome, xdgConfigHome, xdgStateHome string, fileExists func
 
 	// If XDG_STATE_HOME is set to an absolute path, use that
 	if filepath.IsAbs(xdgStateHome) {
-		return filepath.Join(xdgStateHome, "syncthing")
+		return filepath.Join(xdgStateHome, "thingium")
 	}
 
 	// Use our default
@@ -250,7 +250,7 @@ func unixDataDir(userHome, configDir, xdgDataHome, xdgStateHome string, fileExis
 	// that. The variable should be set to an absolute path or be ignored,
 	// but that's not what we did previously, so we retain the old behavior.
 	if xdgDataHome != "" {
-		candidate := filepath.Join(xdgDataHome, "syncthing")
+		candidate := filepath.Join(xdgDataHome, "thingium")
 		if fileExists(filepath.Join(candidate, LevelDBDir)) {
 			return candidate
 		}
@@ -264,7 +264,7 @@ func unixDataDir(userHome, configDir, xdgDataHome, xdgStateHome string, fileExis
 
 	// If XDG_STATE_HOME is set to an absolute path, use that
 	if filepath.IsAbs(xdgStateHome) {
-		return filepath.Join(xdgStateHome, "syncthing")
+		return filepath.Join(xdgStateHome, "thingium")
 	}
 
 	// Use our default
--- lib/locations/locations_test.go
+++ lib/locations/locations_test.go
@@ -28,27 +28,27 @@ func TestUnixConfigDir(t *testing.T) {
 		// First some "new installations", no files exist previously.
 
 		// No variables set, use our current default
-		{"/home/user", "", "", nil, "/home/user/.local/state/syncthing"},
+		{"/home/user", "", "", nil, "/home/user/.local/state/thingium"},
 		// Config home set, doesn't matter
-		{"/home/user", "/somewhere/else", "", nil, "/home/user/.local/state/syncthing"},
+		{"/home/user", "/somewhere/else", "", nil, "/home/user/.local/state/thingium"},
 		// State home set, use that
-		{"/home/user", "", "/var/state", nil, "/var/state/syncthing"},
+		{"/home/user", "", "/var/state", nil, "/var/state/thingium"},
 		// State home set, again config home doesn't matter
-		{"/home/user", "/somewhere/else", "/var/state", nil, "/var/state/syncthing"},
+		{"/home/user", "/somewhere/else", "/var/state", nil, "/var/state/thingium"},
 
 		// Now some "upgrades", where we have files in the old locations.
 
 		// Config home set, a file exists in the default location
-		{"/home/user", "/somewhere/else", "", []string{"/home/user/.config/syncthing/config.xml"}, "/home/user/.config/syncthing"},
+		{"/home/user", "/somewhere/else", "", []string{"/home/user/.config/thingium/config.xml"}, "/home/user/.config/thingium"},
 		// State home set, a file exists in the default location
-		{"/home/user", "", "/var/state", []string{"/home/user/.config/syncthing/config.xml"}, "/home/user/.config/syncthing"},
+		{"/home/user", "", "/var/state", []string{"/home/user/.config/thingium/config.xml"}, "/home/user/.config/thingium"},
 		// Both config home and state home set, a file exists in the default location
-		{"/home/user", "/somewhere/else", "/var/state", []string{"/home/user/.config/syncthing/config.xml"}, "/home/user/.config/syncthing"},
+		{"/home/user", "/somewhere/else", "/var/state", []string{"/home/user/.config/thingium/config.xml"}, "/home/user/.config/thingium"},
 
 		// Config home set, and a file exists at that place
-		{"/home/user", "/somewhere/else", "", []string{"/somewhere/else/syncthing/config.xml"}, "/somewhere/else/syncthing"},
+		{"/home/user", "/somewhere/else", "", []string{"/somewhere/else/thingium/config.xml"}, "/somewhere/else/thingium"},
 		// Config home and state home set, and a file exists in config home
-		{"/home/user", "/somewhere/else", "/var/state", []string{"/somewhere/else/syncthing/config.xml"}, "/somewhere/else/syncthing"},
+		{"/home/user", "/somewhere/else", "/var/state", []string{"/somewhere/else/thingium/config.xml"}, "/somewhere/else/thingium"},
 	}
 
 	for _, c := range cases {
@@ -74,23 +74,23 @@ func TestUnixDataDir(t *testing.T) {
 		// First some "new installations", no files exist previously.
 
 		// No variables set, use our current default
-		{"/home/user", "", "", "", nil, "/home/user/.local/state/syncthing"},
+		{"/home/user", "", "", "", nil, "/home/user/.local/state/thingium"},
 		// Data home set, doesn't matter
-		{"/home/user", "", "/somewhere/else", "", nil, "/home/user/.local/state/syncthing"},
+		{"/home/user", "", "/somewhere/else", "", nil, "/home/user/.local/state/thingium"},
 		// State home set, use that
-		{"/home/user", "", "", "/var/state", nil, "/var/state/syncthing"},
+		{"/home/user", "", "", "/var/state", nil, "/var/state/thingium"},
 
 		// Now some "upgrades", where we have files in the old locations.
 
 		// A database exists in the old default location, use that
-		{"/home/user", "", "", "", []string{"/home/user/.config/syncthing/index-v0.14.0.db"}, "/home/user/.config/syncthing"},
-		{"/home/user", "/config/dir", "/xdg/data/home", "/xdg/state/home", []string{"/home/user/.config/syncthing/index-v0.14.0.db"}, "/home/user/.config/syncthing"},
+		{"/home/user", "", "", "", []string{"/home/user/.config/thingium/index-v0.14.0.db"}, "/home/user/.config/thingium"},
+		{"/home/user", "/config/dir", "/xdg/data/home", "/xdg/state/home", []string{"/home/user/.config/thingium/index-v0.14.0.db"}, "/home/user/.config/thingium"},
 
 		// A database exists in the config dir, use that
 		{"/home/user", "/config/dir", "/xdg/data/home", "/xdg/state/home", []string{"/config/dir/index-v0.14.0.db"}, "/config/dir"},
 
 		// A database exists in the old xdg data home, use that
-		{"/home/user", "/config/dir", "/xdg/data/home", "/xdg/state/home", []string{"/xdg/data/home/syncthing/index-v0.14.0.db"}, "/xdg/data/home/syncthing"},
+		{"/home/user", "/config/dir", "/xdg/data/home", "/xdg/state/home", []string{"/xdg/data/home/thingium/index-v0.14.0.db"}, "/xdg/data/home/thingium"},
 	}
 
 	for _, c := range cases {
