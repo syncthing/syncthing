82380673f08be2e14896656fb58fe1036e070c16
Jakob Borg <jakob@kastelo.net>
2025-03-18 10:56:15 +0100

    wip

--- internal/db/sqlite/db_open_cgo.go
+++ internal/db/sqlite/db_open_cgo.go
@@ -4,14 +4,9 @@ package sqlite
 
 import (
 	_ "github.com/mattn/go-sqlite3" // register sqlite3 database driver
-	"github.com/syncthing/syncthing/lib/build"
 )
 
 const (
 	dbDriver      = "sqlite3"
 	commonOptions = "_fk=true&_rt=true&_cache_size=-65536&_sync=1"
 )
-
-func init() {
-	build.AddTag("mattn-sqlite")
-}
