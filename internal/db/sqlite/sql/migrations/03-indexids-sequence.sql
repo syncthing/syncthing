6f921ad06198847fceb09bab32e7b1b5b9e7aa21
Jakob Borg <jakob@kastelo.net>
2025-03-25 09:46:33 +0100

    wip

--- internal/db/sqlite/sql/migrations/03-indexids-sequence.sql
+++ internal/db/sqlite/sql/migrations/03-indexids-sequence.sql
@@ -1,8 +1,7 @@
 -- add the sequence column to indexids
 ALTER TABLE indexids ADD COLUMN sequence INTEGER NOT NULL DEFAULT 0
 ;
-UPDATE indexids SET sequence = (
-    SELECT COALESCE(MAX(remote_sequence), MAX(sequence)) FROM files
-        WHERE files.device_idx = indexids.device_idx AND files.folder_idx = indexids.folder_idx
-)
+UPDATE indexids SET sequence = ff.sequence
+    FROM (SELECT device_idx, folder_idx, COALESCE(MAX(remote_sequence), MAX(sequence)) AS sequence FROM files GROUP BY device_idx, folder_idx) AS ff
+    WHERE indexids.device_idx = ff.device_idx AND indexids.folder_idx = ff.folder_idx
 ;
