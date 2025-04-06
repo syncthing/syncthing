c585d9a586ee3379dea020889415374539be47cc
mathias4833 <67101597+mathias4833@users.noreply.github.com>
2025-04-06 16:57:25 +0200

    feat(gui): display folder structure as a tree

--- gui/default/syncthing/folder/folderPickerModalController.js
+++ gui/default/syncthing/folder/folderPickerModalController.js
@@ -1,8 +1,9 @@
 angular.module('syncthing.folder')
-    .controller('FolderPickerModalController', function ($scope, $http, $rootScope) {
+    .controller('FolderPickerModalController', function ($scope, $http, $rootScope, $timeout, $translate) {
         'use strict';
 
-        $scope.directories = [];
+        $scope.tree = null;
+        $scope.tempNodes = [];
 
         function addTrailingSeparator(path) {
             if (path.length > 0 && !path.endsWith($scope.pathSeparator)) {
@@ -18,50 +19,153 @@ angular.module('syncthing.folder')
             return path;
         }
 
-        function getParentPath(path) {
-            let parts = stripTrailingSeparator(path).split($scope.pathSeparator);
-            parts.pop();
-
-            if (parts.length === 0) return '';
-            return parts.join($scope.pathSeparator) + $scope.pathSeparator;
+        function expandTilde(path) {
+            if (path.startsWith('~')) {
+                return path.replace('~', $scope.$parent.system.tilde);
+            }
+            return path;
         }
 
-        $scope.formatDirectoryName = function (path) {
-            return path.split($scope.pathSeparator).filter(Boolean).pop() || path;
-        };
+        function splitPath(path) {
+            const trimmedPath = expandTilde(stripTrailingSeparator(path));
+            // Keep the leading separator if it exists
+            let parts = trimmedPath.startsWith($scope.pathSeparator)
+                ? [$scope.pathSeparator, ...trimmedPath.slice($scope.pathSeparator.length).split($scope.pathSeparator)]
+                : trimmedPath.split($scope.pathSeparator);
 
-        $scope.updateDirectoryList = function () {
-            $http.get(urlbase + '/system/browse', {
-                params: {
-                    current: $scope.currentPath
+            return parts.filter(Boolean);
+        }
+
+        function joinPath(...parts) {
+            // Don't add a second separator if the first part is already a separator
+            if (parts.length > 0 && parts[0] === $scope.pathSeparator) {
+                return $scope.pathSeparator + parts.slice(1).join($scope.pathSeparator);
+            }
+            return parts.join($scope.pathSeparator);
+        }
+
+        function formatDirectoryName(path) {
+            return splitPath(path).pop() || path
+        }
+
+        async function fetchSubdirectories(path) {
+            let res = await $http.get(urlbase + '/system/browse', {
+                params: {current: addTrailingSeparator(path)}
+            });
+            return res.data.map(dir => ({
+                title: formatDirectoryName(dir),
+                key: stripTrailingSeparator(dir),
+                folder: true,
+                lazy: true
+            }));
+        }
+
+        function clearTemporaryNodes(currentNode) {
+            // Delete temporary nodes, children first
+            $scope.tempNodes.sort((a, b) => b.getLevel() - a.getLevel());
+            $scope.tempNodes = $scope.tempNodes.filter(node => {
+                if (!node.children && node.key !== currentNode.key) {
+                    node.remove();
+                    return false;
                 }
-            }).success(function (data) {
-                $scope.directories = data.map(addTrailingSeparator);
-            }).error($scope.emitHTTPError);
-        };
+                return true;
+            });
+        }
 
-        $scope.navigateTo = function (path) {
-            $scope.currentPath = path;
-            $scope.updateDirectoryList();
-        };
+        async function findOrCreateNode(currentNode, key) {
+            if (currentNode.isLazy()) {
+                await currentNode.load();
+            }
+            if (!currentNode.children) {
+                currentNode.children = [];
+            }
 
-        $scope.navigateUp = function () {
-            $scope.currentPath = getParentPath($scope.currentPath);
-            $scope.updateDirectoryList()
-        };
+            let nextNode = currentNode.children.find(child => child.title === key);
+            // Prevent creating temp nodes at the root of the tree
+            if (!nextNode && currentNode !== $scope.tree.getRootNode()) {
+                nextNode = currentNode.addChildren({
+                    title: key,
+                    key: joinPath(currentNode.key, key),
+                    extraClasses: "fancytree-temp",
+                    folder: true,
+                    tooltip: $translate.instant('Folder will be created')
+                });
+                currentNode.sortChildren();
+                $scope.tempNodes.push(nextNode);
+            }
+            return nextNode;
+        }
 
-        $scope.selectCurrentPath = function () {
-            $rootScope.$emit('folderPathSelected', stripTrailingSeparator($scope.currentPath));
-            angular.element('#folderPicker').modal('hide');
-        };
+        function handleNodeActivation(node) {
+            clearTemporaryNodes(node);
+            if (node.key === expandTilde(stripTrailingSeparator($scope.currentPath))) return;
+
+            $scope.$apply(() => {
+                $scope.currentPath = node.key;
+            });
+        }
+
+        async function initFolderTree() {
+            const rootDirs = await fetchSubdirectories('');
+
+            $("#folderTree").fancytree({
+                extensions: ["table", "glyph"],
+                quicksearch: true,
+                glyph: {
+                    preset: "awesome5",
+                    map: {
+                        expanderLazy: "fa fa-caret-right",
+                    }
+                },
+                table: {
+                    indentation: 24,
+                },
+                strings: {
+                    loading: $translate.instant("Loading data..."),
+                    loadError: $translate.instant("Failed to load data"),
+                },
+                debugLevel: 1,
+                selectMode: 1,
+                source: rootDirs,
+                lazyLoad: function (event, data) {
+                    data.result = fetchSubdirectories(data.node.key);
+                },
+                activate: (event, data) => handleNodeActivation(data.node),
+            });
+
+            $scope.tree = $.ui.fancytree.getTree("#folderTree");
+        }
+
+        $scope.selectNodeByCurrentPath = async function () {
+            if (!$scope.tree || !$scope.currentPath) return;
+
+            const parts = splitPath($scope.currentPath);
+
+            let currentNode = $scope.tree.getRootNode();
+            for (let part of parts) {
+                currentNode = await findOrCreateNode(currentNode, part);
+                if (!currentNode) return;
+            }
+
+            await currentNode.makeVisible();
+            await currentNode.setExpanded(true);
+            await currentNode.setActive(true);
+        }
 
         angular.element("#folderPicker").on("shown.bs.modal", function () {
-            $scope.$apply(() => {
-                $scope.pathSeparator = $scope.$parent.system.pathSeparator || '/';
-                $scope.currentPath = $scope.$parent.currentFolder.path || '';
-                $scope.updateDirectoryList();
-            });
+            $scope.pathSeparator = $scope.$parent.system.pathSeparator || '/';
+            $scope.currentPath = $scope.$parent.currentFolder.path || '';
+
+            $timeout(async () => {
+                if (!$scope.tree) {
+                    await initFolderTree();
+                }
+                await $scope.selectNodeByCurrentPath();
+            })
         });
 
-        angular.element("#folderPickerSelect").on("click", $scope.selectCurrentPath)
+        angular.element("#folderPickerSelect").on("click", () => {
+            $rootScope.$emit('folderPathSelected', stripTrailingSeparator($scope.currentPath));
+            angular.element('#folderPicker').modal('hide');
+        }) ;
     });
