44f70463221fc79c42e579730aa13d5c6861362a
Jakob Borg <jakob@kastelo.net>
2025-05-25 08:01:50 +0200

    fix(syncthing): ensure both config and data dirs exist at startup
    Previously we'd only ensure the config dir, which is often but not
    always the same as the data dir.

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -349,10 +349,12 @@ func (options serveOptions) Run() error {
 		return nil
 	}
 
-	// Ensure that our home directory exists.
-	if err := syncthing.EnsureDir(locations.GetBaseDir(locations.ConfigBaseDir), 0o700); err != nil {
-		l.Warnln("Failure on home directory:", err)
-		os.Exit(svcutil.ExitError.AsInt())
+	// Ensure that our config and data directories exist.
+	for _, loc := range []locations.BaseDirEnum{locations.ConfigBaseDir, locations.DataBaseDir} {
+		if err := syncthing.EnsureDir(locations.GetBaseDir(loc), 0o700); err != nil {
+			l.Warnln("Failed to ensure directory exists:", err)
+			os.Exit(svcutil.ExitError.AsInt())
+		}
 	}
 
 	if options.UpgradeTo != "" {
