f68985714d4f0dcb99ffe6b1a34543cbd0a1b1b4
ardevd <ardevd@protonmail.com>
2025-06-09 21:14:01 +0200

    refactor(syncthing): use named constant for SIGHUP syscall.
    It’s more self-documenting and avoids confusion if syscall numbers change on some platform.

--- cmd/syncthing/main.go
+++ cmd/syncthing/main.go
@@ -611,8 +611,7 @@ func setupSignalHandling(app *syncthing.App) {
 	// Exit cleanly with "restarting" code on SIGHUP.
 
 	restartSign := make(chan os.Signal, 1)
-	sigHup := syscall.Signal(1)
-	signal.Notify(restartSign, sigHup)
+	signal.Notify(restartSign, syscall.SIGHUP)
 	go func() {
 		<-restartSign
 		app.Stop(svcutil.ExitRestart)
