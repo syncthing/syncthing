4d05dc9a803ebf1d6bddf28fcf379ae57a6a3454
largemouth <largemouth@aliyun.com>
2025-06-17 18:02:30 +0800

    chore: fix function name
    Signed-off-by: largemouth <largemouth@aliyun.com>

--- script/authors.go
+++ script/authors.go
@@ -171,7 +171,7 @@ var excludeCommits = stringSetFromStrings([]string{
 	"4dfb9d7c83ed172f12ae19408517961f4a49beeb",
 })
 
-// allAuthors returns the set of authors in the git commit log, except those
+// addAuthors returns the set of authors in the git commit log, except those
 // in excluded commits.
 func addAuthors(authors *authorSet) {
 	// All existing source-tracked files
