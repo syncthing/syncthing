2e5059f95877d9c6654943d67b9d1924466d1a0c
Daniil Gentili <daniil@daniil.it>
2025-07-29 14:53:50 +0200

    Allow disabling scan progress events if there are too many files

--- lib/config/folderconfiguration.go
+++ lib/config/folderconfiguration.go
@@ -48,46 +48,47 @@ type FolderDeviceConfiguration struct {
 }
 
 type FolderConfiguration struct {
-	ID                      string                      `json:"id" xml:"id,attr" nodefault:"true"`
-	Label                   string                      `json:"label" xml:"label,attr" restart:"false"`
-	FilesystemType          FilesystemType              `json:"filesystemType" xml:"filesystemType" default:"basic"`
-	Path                    string                      `json:"path" xml:"path,attr"`
-	Type                    FolderType                  `json:"type" xml:"type,attr"`
-	Devices                 []FolderDeviceConfiguration `json:"devices" xml:"device"`
-	RescanIntervalS         int                         `json:"rescanIntervalS" xml:"rescanIntervalS,attr" default:"3600"`
-	FSWatcherEnabled        bool                        `json:"fsWatcherEnabled" xml:"fsWatcherEnabled,attr" default:"true"`
-	FSWatcherDelayS         float64                     `json:"fsWatcherDelayS" xml:"fsWatcherDelayS,attr" default:"10"`
-	FSWatcherTimeoutS       float64                     `json:"fsWatcherTimeoutS" xml:"fsWatcherTimeoutS,attr"`
-	IgnorePerms             bool                        `json:"ignorePerms" xml:"ignorePerms,attr"`
-	AutoNormalize           bool                        `json:"autoNormalize" xml:"autoNormalize,attr" default:"true"`
-	MinDiskFree             Size                        `json:"minDiskFree" xml:"minDiskFree" default:"1 %"`
-	Versioning              VersioningConfiguration     `json:"versioning" xml:"versioning"`
-	Copiers                 int                         `json:"copiers" xml:"copiers"`
-	PullerMaxPendingKiB     int                         `json:"pullerMaxPendingKiB" xml:"pullerMaxPendingKiB"`
-	Hashers                 int                         `json:"hashers" xml:"hashers"`
-	Order                   PullOrder                   `json:"order" xml:"order"`
-	IgnoreDelete            bool                        `json:"ignoreDelete" xml:"ignoreDelete"`
-	ScanProgressIntervalS   int                         `json:"scanProgressIntervalS" xml:"scanProgressIntervalS"`
-	PullerPauseS            int                         `json:"pullerPauseS" xml:"pullerPauseS"`
-	MaxConflicts            int                         `json:"maxConflicts" xml:"maxConflicts" default:"10"`
-	DisableSparseFiles      bool                        `json:"disableSparseFiles" xml:"disableSparseFiles"`
-	DisableTempIndexes      bool                        `json:"disableTempIndexes" xml:"disableTempIndexes"`
-	Paused                  bool                        `json:"paused" xml:"paused"`
-	WeakHashThresholdPct    int                         `json:"weakHashThresholdPct" xml:"weakHashThresholdPct"`
-	MarkerName              string                      `json:"markerName" xml:"markerName"`
-	CopyOwnershipFromParent bool                        `json:"copyOwnershipFromParent" xml:"copyOwnershipFromParent"`
-	RawModTimeWindowS       int                         `json:"modTimeWindowS" xml:"modTimeWindowS"`
-	MaxConcurrentWrites     int                         `json:"maxConcurrentWrites" xml:"maxConcurrentWrites" default:"2"`
-	DisableFsync            bool                        `json:"disableFsync" xml:"disableFsync"`
-	BlockPullOrder          BlockPullOrder              `json:"blockPullOrder" xml:"blockPullOrder"`
-	CopyRangeMethod         CopyRangeMethod             `json:"copyRangeMethod" xml:"copyRangeMethod" default:"standard"`
-	CaseSensitiveFS         bool                        `json:"caseSensitiveFS" xml:"caseSensitiveFS"`
-	JunctionsAsDirs         bool                        `json:"junctionsAsDirs" xml:"junctionsAsDirs"`
-	SyncOwnership           bool                        `json:"syncOwnership" xml:"syncOwnership"`
-	SendOwnership           bool                        `json:"sendOwnership" xml:"sendOwnership"`
-	SyncXattrs              bool                        `json:"syncXattrs" xml:"syncXattrs"`
-	SendXattrs              bool                        `json:"sendXattrs" xml:"sendXattrs"`
-	XattrFilter             XattrFilter                 `json:"xattrFilter" xml:"xattrFilter"`
+	ID                                string                      `json:"id" xml:"id,attr" nodefault:"true"`
+	Label                             string                      `json:"label" xml:"label,attr" restart:"false"`
+	FilesystemType                    FilesystemType              `json:"filesystemType" xml:"filesystemType" default:"basic"`
+	Path                              string                      `json:"path" xml:"path,attr"`
+	Type                              FolderType                  `json:"type" xml:"type,attr"`
+	Devices                           []FolderDeviceConfiguration `json:"devices" xml:"device"`
+	RescanIntervalS                   int                         `json:"rescanIntervalS" xml:"rescanIntervalS,attr" default:"3600"`
+	FSWatcherEnabled                  bool                        `json:"fsWatcherEnabled" xml:"fsWatcherEnabled,attr" default:"true"`
+	FSWatcherDelayS                   float64                     `json:"fsWatcherDelayS" xml:"fsWatcherDelayS,attr" default:"10"`
+	FSWatcherTimeoutS                 float64                     `json:"fsWatcherTimeoutS" xml:"fsWatcherTimeoutS,attr"`
+	IgnorePerms                       bool                        `json:"ignorePerms" xml:"ignorePerms,attr"`
+	AutoNormalize                     bool                        `json:"autoNormalize" xml:"autoNormalize,attr" default:"true"`
+	MinDiskFree                       Size                        `json:"minDiskFree" xml:"minDiskFree" default:"1 %"`
+	Versioning                        VersioningConfiguration     `json:"versioning" xml:"versioning"`
+	Copiers                           int                         `json:"copiers" xml:"copiers"`
+	PullerMaxPendingKiB               int                         `json:"pullerMaxPendingKiB" xml:"pullerMaxPendingKiB"`
+	Hashers                           int                         `json:"hashers" xml:"hashers"`
+	Order                             PullOrder                   `json:"order" xml:"order"`
+	IgnoreDelete                      bool                        `json:"ignoreDelete" xml:"ignoreDelete"`
+	ScanProgressIntervalS             int                         `json:"scanProgressIntervalS" xml:"scanProgressIntervalS"`
+	ScanProgressEnableIfFilesLessThan int                         `json:"scanProgressEnableIfFilesLessThan" xml:"scanProgressEnableIfFilesLessThan"`
+	PullerPauseS                      int                         `json:"pullerPauseS" xml:"pullerPauseS"`
+	MaxConflicts                      int                         `json:"maxConflicts" xml:"maxConflicts" default:"10"`
+	DisableSparseFiles                bool                        `json:"disableSparseFiles" xml:"disableSparseFiles"`
+	DisableTempIndexes                bool                        `json:"disableTempIndexes" xml:"disableTempIndexes"`
+	Paused                            bool                        `json:"paused" xml:"paused"`
+	WeakHashThresholdPct              int                         `json:"weakHashThresholdPct" xml:"weakHashThresholdPct"`
+	MarkerName                        string                      `json:"markerName" xml:"markerName"`
+	CopyOwnershipFromParent           bool                        `json:"copyOwnershipFromParent" xml:"copyOwnershipFromParent"`
+	RawModTimeWindowS                 int                         `json:"modTimeWindowS" xml:"modTimeWindowS"`
+	MaxConcurrentWrites               int                         `json:"maxConcurrentWrites" xml:"maxConcurrentWrites" default:"2"`
+	DisableFsync                      bool                        `json:"disableFsync" xml:"disableFsync"`
+	BlockPullOrder                    BlockPullOrder              `json:"blockPullOrder" xml:"blockPullOrder"`
+	CopyRangeMethod                   CopyRangeMethod             `json:"copyRangeMethod" xml:"copyRangeMethod" default:"standard"`
+	CaseSensitiveFS                   bool                        `json:"caseSensitiveFS" xml:"caseSensitiveFS"`
+	JunctionsAsDirs                   bool                        `json:"junctionsAsDirs" xml:"junctionsAsDirs"`
+	SyncOwnership                     bool                        `json:"syncOwnership" xml:"syncOwnership"`
+	SendOwnership                     bool                        `json:"sendOwnership" xml:"sendOwnership"`
+	SyncXattrs                        bool                        `json:"syncXattrs" xml:"syncXattrs"`
+	SendXattrs                        bool                        `json:"sendXattrs" xml:"sendXattrs"`
+	XattrFilter                       XattrFilter                 `json:"xattrFilter" xml:"xattrFilter"`
 	// Legacy deprecated
 	DeprecatedReadOnly       bool    `json:"-" xml:"ro,attr,omitempty"`        // Deprecated: Do not use.
 	DeprecatedMinDiskFreePct float64 `json:"-" xml:"minDiskFreePct,omitempty"` // Deprecated: Do not use.
