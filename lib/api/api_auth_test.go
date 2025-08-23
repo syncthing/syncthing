5c30669e85e24d8ba91b60ef3837b66513dd35af
Simon Frei <freisim93@gmail.com>
2025-08-23 16:07:49 +0200

    chore(db): cleanup DB in tests and remove OpenTemp
    Filled up my tmpfs with tests DBs when running benchmarks :)

--- lib/api/api_auth_test.go
+++ lib/api/api_auth_test.go
@@ -130,7 +130,7 @@ func (c *mockClock) wind(t time.Duration) {
 func TestTokenManager(t *testing.T) {
 	t.Parallel()
 
-	mdb, err := sqlite.OpenTemp()
+	mdb, err := sqlite.Open(t.TempDir())
 	if err != nil {
 		t.Fatal(err)
 	}
