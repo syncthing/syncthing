fdefed623d96e241420034b9cae7edef284eb25b
Jakob Borg <jakob@kastelo.net>
2025-08-29 15:49:21 +0200

    wip

--- internal/db/sqlite/sql/migrations/folder/04-alter-blocks-tables.sql
+++ internal/db/sqlite/sql/migrations/folder/04-alter-blocks-tables.sql
@@ -4,18 +4,20 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this file,
 -- You can obtain one at https://mozilla.org/MPL/2.0/.
 
+-- Copy blocks to new table with fewer indexes
+
 CREATE TABLE IF NOT EXISTS blocks_v4 (
     hash BLOB NOT NULL,
     blocklist_hash BLOB NOT NULL,
     idx INTEGER NOT NULL,
     offset INTEGER NOT NULL,
     size INTEGER NOT NULL,
-    PRIMARY KEY(hash, blocklist_hash, idx)
+    PRIMARY KEY (hash, blocklist_hash, idx)
 ) STRICT, WITHOUT ROWID
 ;
 
-INSERT INTO blocks_v4 (hash, blocklist_hash, idx, offset, primary)
-SELECT hash, blocklist_hash, idx, offset, primary FROM blocklists
+INSERT INTO blocks_v4 (hash, blocklist_hash, idx, offset, size)
+SELECT hash, blocklist_hash, idx, offset, size FROM blocks
 ;
 
 DROP TABLE blocks
@@ -23,6 +25,8 @@ DROP TABLE blocks
 
 ALTER TABLE blocks_v4 RENAME TO blocks
 ;
+
+-- Copy blocklists to new table with fewer indexes
 
 CREATE TABLE IF NOT EXISTS blocklists_v4 (
     blocklist_hash BLOB NOT NULL PRIMARY KEY,
