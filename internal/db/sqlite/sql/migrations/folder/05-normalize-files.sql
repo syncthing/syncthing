f58a4195c84fa7aa1a04bc55d7fa877240629dc5
Jakob Borg <jakob@kastelo.net>
2025-09-12 11:20:24 +0200

    Update internal/db/sqlite/sql/migrations/folder/05-normalize-files.sql
    Co-authored-by: Ross Smith II <ross@smithii.com>
    Signed-off-by: Jakob Borg <jakob@kastelo.net>

--- internal/db/sqlite/sql/migrations/folder/05-normalize-files.sql
+++ internal/db/sqlite/sql/migrations/folder/05-normalize-files.sql
@@ -4,7 +4,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this file,
 -- You can obtain one at https://mozilla.org/MPL/2.0/.
 
--- Grab all uniqe names into the names table
+-- Grab all unique names into the names table
 
 INSERT INTO file_names (idx, name) SELECT DISTINCT null, name FROM files
 ;
