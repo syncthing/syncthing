e9331a633417ee9c66e0456db893053a4d800c31
bxff <51504045+bxff@users.noreply.github.com>
2025-12-31 23:59:57 +0530

    fix: license

--- lib/model/folder.go
+++ lib/model/folder.go
@@ -1,8 +1,8 @@
-// Copyright (C) 2014 The Syncthing Authors.
+// Copyright (C) 2025 The Syncthing Authors & bxff
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
-// You can obtain one at https://mozilla.org/MPL/2.0/.
+// You can obtain one at https://mozilla.org/MPL/2.0/
 
 package model
 
--- lib/osutil/symlinkcache.go
+++ lib/osutil/symlinkcache.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 The Syncthing Authors.
+// Copyright (C) 2025 The Syncthing Authors & bxff
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
