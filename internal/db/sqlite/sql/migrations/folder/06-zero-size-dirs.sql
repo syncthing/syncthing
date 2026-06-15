439affa7a2e1ac0f98f222ba1082c0473d77d41a
Simon Frei <freisim93@gmail.com>
2026-06-15 23:20:59 +0200

    set size zero for all non-file-type files
    Signed-off-by: Simon Frei <freisim93@gmail.com>

--- internal/db/sqlite/sql/migrations/folder/06-zero-size-dirs.sql
+++ internal/db/sqlite/sql/migrations/folder/06-zero-size-dirs.sql
@@ -4,8 +4,9 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this file,
 -- You can obtain one at https://mozilla.org/MPL/2.0/.
 
--- Directories were previously stored with a "synthetic" size of 128
+-- All non-file entries should have size zero.
+-- Directories were previously stored with a "synthetic" size of 128.
 UPDATE files
     SET size = 0
-    WHERE type = 1
+    WHERE type != 0
 ;
